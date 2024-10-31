5ff50a9fb92f8e80f8b4f41d38152aa26e6037ca
Paul Lam <paul@quantisan.com>
2024-10-31 09:21:41 +0900

    add uniqueness_workflow.py stub

---
+++ docs/docusaurus/docs/reference/learn/data_quality_use_cases/uniqueness_resources/uniqueness_workflow.py
@@ -0,0 +1,40 @@
+"""
+To run this test locally, use the postgresql database docker container.
+
+1. From the repo root dir, run:
+cd assets/docker/postgresql
+docker compose up
+
+2. Run the following command from the repo root dir in a second terminal:
+pytest --postgresql --docs-tests -k "data_quality_use_case_integration_workflow" tests/integration/test_script_runner.py
+"""
+
+# <snippet name="docs/docusaurus/docs/reference/learn/data_quality_use_cases/uniqueness_resources/uniqueness_workflow.py full workflow">
+import great_expectations as gx
+import great_expectations.expectations as gxe
+
+# Create Data Context.
+context = gx.get_context()
+
+# Connect to data.
+# Create Data Source, Data Asset, Batch Definition, and Batch.
+CONNECTION_STRING = "postgresql+psycopg2://try_gx:try_gx@postgres.workshops.greatexpectations.io/gx_learn_data_quality"
+
+data_source = context.data_sources.add_postgres(
+    "postgres database", connection_string=CONNECTION_STRING
+)
+data_asset = data_source.add_table_asset(
+    name="purchases", table_name="uniqueness_customers"
+)
+
+batch_definition = data_asset.add_batch_definition_whole_table("batch definition")
+batch = batch_definition.get_batch()
+
+# Create an Expectation Suite containing distribution Expectations.
+expectation_suite = context.suites.add(
+    gx.core.expectation_suite.ExpectationSuite(name="expectation suite")
+)
+
+# Validate Batch using Expectation.
+validation_result = batch.validate(expectation_suite)
+# </snippet>
