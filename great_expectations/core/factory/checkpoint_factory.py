098aa5344aa1317683890970c60cb3b72e56b721
Chetan Kini <chetan@superconductive.com>
2024-10-31 11:47:07 -0400

    use isinstance

--- great_expectations/core/factory/checkpoint_factory.py
+++ great_expectations/core/factory/checkpoint_factory.py
@@ -9,6 +9,7 @@ from great_expectations.analytics.events import (
     CheckpointCreatedEvent,
     CheckpointDeletedEvent,
 )
+from great_expectations.checkpoint.actions import UpdateDataDocsAction
 from great_expectations.checkpoint.checkpoint import Checkpoint
 from great_expectations.compatibility.typing_extensions import override
 from great_expectations.core.factory.factory import Factory
@@ -60,7 +61,9 @@ class CheckpointFactory(Factory[Checkpoint]):
                     ActionInfo(
                         type=action.type,
                         # notify_on is not a property of all Actions
-                        notify_on=getattr(action, "notify_on", None),
+                        notify_on=action.notify_on
+                        if not isinstance(action, UpdateDataDocsAction)
+                        else None,
                     )
                     for action in checkpoint.actions
                 ],
