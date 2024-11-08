2cab6a54df19a7166763458229aaafdc3558f86f
Chetan Kini <chetan@superconductive.com>
2024-11-08 09:00:39 -0500

    fix

--- tests/actions/test_core_actions.py
+++ tests/actions/test_core_actions.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime as dt
 import json
 import logging
 from contextlib import contextmanager
@@ -30,6 +31,7 @@ from great_expectations.core.expectation_validation_result import (
     ExpectationSuiteValidationResult,
 )
 from great_expectations.core.run_identifier import RunIdentifier
+from great_expectations.core.validation_definition import ValidationDefinition
 from great_expectations.data_context.cloud_constants import GXCloudRESTResource
 from great_expectations.data_context.data_context.abstract_data_context import (
     AbstractDataContext,
@@ -1039,3 +1041,72 @@ class TestV1ActionRun:
         assert mock_context.config_provider.substitute_config.call_count == 2
         mock_context.config_provider.substitute_config.assert_any_call("${SLACK_CHANNEL}")
         mock_context.config_provider.substitute_config.assert_any_call("${SLACK_TOKEN}")
+
+
+class TestActionIntegration:
+    """
+    Tests for actions that require external services to be available.
+    """
+
+    @pytest.fixture
+    def checkpoint_result(self, mocker) -> CheckpointResult:
+        return CheckpointResult(
+            run_id=RunIdentifier(run_name="e2e_test", run_time=dt.datetime.now(tz=dt.timezone.utc)),
+            run_results={
+                ValidationResultIdentifier(
+                    expectation_suite_identifier=ExpectationSuiteIdentifier(
+                        name="test-suite",
+                    ),
+                    run_id=RunIdentifier(run_name="test-run"),
+                    batch_identifier="test-datasource-test-asset",
+                ): ExpectationSuiteValidationResult(
+                    success=True,
+                    statistics={"successful_expectations": 3, "evaluated_expectations": 3},
+                    results=[],
+                    suite_name="test-suite",
+                ),
+                ValidationResultIdentifier(
+                    expectation_suite_identifier=ExpectationSuiteIdentifier(
+                        name="test-suite",
+                    ),
+                    run_id=RunIdentifier(run_name="test-run"),
+                    batch_identifier="test-datasource-test-asset",
+                ): ExpectationSuiteValidationResult(
+                    success=False,
+                    statistics={"successful_expectations": 2, "evaluated_expectations": 4},
+                    results=[],
+                    suite_name="test-suite",
+                ),
+            },
+            checkpoint_config=Checkpoint(
+                name="test-checkpoint",
+                validation_definitions=[
+                    mocker.MagicMock(spec=ValidationDefinition),
+                    mocker.MagicMock(spec=ValidationDefinition),
+                ],
+            ),
+            success=False,
+        )
+
+    @pytest.mark.integration
+    @pytest.mark.parametrize(
+        "action_cls, action_creds, expected_action_result",
+        [
+            pytest.param(
+                MicrosoftTeamsNotificationAction,
+                {"teams_webhook": "${GX_MS_TEAMS_WEBHOOK}"},
+                {"microsoft_teams_notification_result": "Microsoft Teams notification succeeded."},
+                id="ms_teams",
+            ),
+        ],
+    )
+    def test_actions(
+        self,
+        action_cls: type[ValidationAction],
+        action_creds: dict,
+        expected_action_result: dict,
+        checkpoint_result: CheckpointResult,
+    ):
+        action = action_cls(name="test-action", **action_creds)
+        result = action.run(checkpoint_result=checkpoint_result)
+        assert result == expected_action_result
