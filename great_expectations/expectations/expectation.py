105bdd36b0be61d41ff424b72b975233f895c229
Farmer <nathan@greatexpectations.io>
2024-11-14 11:55:57 -0700

    Use type alias

--- great_expectations/expectations/expectation.py
+++ great_expectations/expectations/expectation.py
@@ -65,6 +65,7 @@ from great_expectations.expectations.model_field_descriptions import (
     WINDOWS_DESCRIPTION,
 )
 from great_expectations.expectations.model_field_types import (
+    ConditionParser,
     ConditionParserEnum,
     ConditionParserException,
     MostlyField,
@@ -1792,7 +1793,7 @@ class ColumnAggregateExpectation(BatchExpectation, ABC):
 
     column: StrictStr = Field(min_length=1, description=COLUMN_DESCRIPTION)
     row_condition: Union[str, None] = None
-    condition_parser: Union[Literal["pandas", "great_expectations"], None] = None
+    condition_parser: Union[ConditionParser, None] = None
 
     domain_keys: ClassVar[Tuple[str, ...]] = (
         "batch_id",
@@ -1852,7 +1853,7 @@ class ColumnMapExpectation(BatchExpectation, ABC):
     column: StrictStr = Field(min_length=1, description=COLUMN_DESCRIPTION)
     mostly: MostlyField = 1
     row_condition: Union[str, None] = None
-    condition_parser: Union[Literal["pandas", "great_expectations"], None] = None
+    condition_parser: Union[ConditionParser, None] = None
 
     catch_exceptions: bool = True
 
@@ -2127,7 +2128,7 @@ class ColumnPairMapExpectation(BatchExpectation, ABC):
     column_B: StrictStr = Field(min_length=1, description=COLUMN_B_DESCRIPTION)
     mostly: MostlyField = 1
     row_condition: Union[str, None] = None
-    condition_parser: Union[Literal["pandas", "great_expectations"], None] = None
+    condition_parser: Union[ConditionParser, None] = None
 
     catch_exceptions: bool = True
 
@@ -2390,7 +2391,7 @@ class MulticolumnMapExpectation(BatchExpectation, ABC):
     column_list: List[StrictStr] = pydantic.Field(description=COLUMN_LIST_DESCRIPTION)
     mostly: MostlyField = 1
     row_condition: Union[str, None] = None
-    condition_parser: Union[Literal["pandas", "great_expectations"], None] = None
+    condition_parser: Union[ConditionParser, None] = None
     ignore_row_if: Literal["all_values_are_missing", "any_value_is_missing", "never"] = (
         "all_values_are_missing"
     )
