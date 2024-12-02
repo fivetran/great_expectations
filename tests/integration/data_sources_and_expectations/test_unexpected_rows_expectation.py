0f82a09345eddddeced8587d1af34f0111e44424
Farmer <nathan@greatexpectations.io>
2024-12-02 15:52:18 -0700

    Make test failable

--- tests/integration/data_sources_and_expectations/test_unexpected_rows_expectation.py
+++ tests/integration/data_sources_and_expectations/test_unexpected_rows_expectation.py
@@ -32,10 +32,24 @@ ALL_DATA_SOURCES: Sequence[DataSourceTestConfig] = [
     data_source_configs=ALL_DATA_SOURCES,
     data=pd.DataFrame({"a": [1, 2]}),
 )
-def test_unexpected_rows_expectation_batch_keyword(batch_for_datasource) -> None:
+def test_unexpected_rows_expectation_batch_keyword_success(batch_for_datasource) -> None:
+    unexpected_rows_query = "SELECT * FROM {batch} WHERE a > 2"
+    expectation = gxe.UnexpectedRowsExpectation(
+        description="Expect a to be less than 3", unexpected_rows_query=unexpected_rows_query
+    )
+    result = batch_for_datasource.validate(expectation)
+    assert result.success
+
+
+@parameterize_batch_for_data_sources(
+    data_source_configs=ALL_DATA_SOURCES,
+    data=pd.DataFrame({"a": [1, 2]}),
+)
+def test_unexpected_rows_expectation_batch_keyword_failure(batch_for_datasource) -> None:
     unexpected_rows_query = "SELECT * FROM {batch}"
     expectation = gxe.UnexpectedRowsExpectation(
         description="Expect table to be empty", unexpected_rows_query=unexpected_rows_query
     )
     result = batch_for_datasource.validate(expectation)
-    assert not result.success
+    assert result.success is False
+    assert result.exception_info.get("raised_exception") is False
