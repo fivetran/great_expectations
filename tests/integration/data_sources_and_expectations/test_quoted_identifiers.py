7c0ba70b51a0324de1593c26499f329a5856d44b
Chetan Kini <chetan@superconductive.com>
2024-12-03 15:27:22 -0500

    misc updates

--- tests/integration/data_sources_and_expectations/test_quoted_identifiers.py
+++ tests/integration/data_sources_and_expectations/test_quoted_identifiers.py
@@ -1,147 +1,259 @@
-# TODO: Write tests
-
-
-from typing import TYPE_CHECKING, Final, Literal, Mapping, Sequence, Type
+from typing import Final, Literal, Mapping
 
 import pandas as pd
+from typing_extensions import TypeAlias
 
+import great_expectations.expectations as gxe
 from great_expectations.datasource.fluent.interfaces import Batch
 from tests.integration.conftest import parameterize_batch_for_data_sources
 from tests.integration.test_utils.data_source_config import (
     DatabricksDatasourceTestConfig,
-    DataSourceTestConfig,
     PostgreSQLDatasourceTestConfig,
     SnowflakeDatasourceTestConfig,
     SqliteDatasourceTestConfig,
 )
-
-if TYPE_CHECKING:
-    from typing_extensions import TypeAlias
-
-SUPPORTED_DATA_SOURCE_TYPES: Sequence[Type[DataSourceTestConfig]] = [
-    DatabricksDatasourceTestConfig,
-    PostgreSQLDatasourceTestConfig,
-    SnowflakeDatasourceTestConfig,
-    SqliteDatasourceTestConfig,
-]
-
-TEST_TABLE_NAME: Final[Literal["test_table"]] = "test_table"
-
-DatabaseType: TypeAlias = Literal["databricks", "postgresql", "snowflake", "sqlite"]
-TableNameCase: TypeAlias = Literal[
-    "quoted_lower",
-    "quoted_mixed",
-    "quoted_upper",
-    "unquoted_lower",
-    "unquoted_mixed",
-    "unquoted_upper",
-]
-
-TABLE_NAME_MAPPING: Final[Mapping[DatabaseType, Mapping[TableNameCase, str]]] = {
-    "postgres": {
-        "unquoted_lower": TEST_TABLE_NAME.lower(),
-        "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
-        "unquoted_upper": TEST_TABLE_NAME.upper(),
-        "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
-        "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
-        "unquoted_mixed": TEST_TABLE_NAME.title(),
-    },
-    "databricks": {
-        "unquoted_lower": TEST_TABLE_NAME.lower(),
-        "quoted_lower": f"`{TEST_TABLE_NAME.lower()}`",
-        "unquoted_upper": TEST_TABLE_NAME.upper(),
-        "quoted_upper": f"`{TEST_TABLE_NAME.upper()}`",
-        "quoted_mixed": f"`{TEST_TABLE_NAME.title()}`",
-        "unquoted_mixed": TEST_TABLE_NAME.title(),
-    },
-    "snowflake": {
-        "unquoted_lower": TEST_TABLE_NAME.lower(),
-        "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
-        "unquoted_upper": TEST_TABLE_NAME.upper(),
-        "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
-        "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
-        "unquoted_mixed": TEST_TABLE_NAME.title(),
-    },
-    "sqlite": {
-        "unquoted_lower": TEST_TABLE_NAME.lower(),
-        "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
-        "unquoted_upper": TEST_TABLE_NAME.upper(),
-        "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
-        "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
-        "unquoted_mixed": TEST_TABLE_NAME.title(),
-    },
-}
-
-# Column names
-UNQUOTED_UPPER_COL: Final[Literal["UNQUOTED_UPPER_COL"]] = "UNQUOTED_UPPER_COL"
-UNQUOTED_LOWER_COL: Final[Literal["unquoted_lower_col"]] = "unquoted_lower_col"
-QUOTED_UPPER_COL: Final[Literal["QUOTED_UPPER_COL"]] = "QUOTED_UPPER_COL"
-QUOTED_LOWER_COL: Final[Literal["quoted_lower_col"]] = "quoted_lower_col"
-QUOTED_MIXED_CASE: Final[Literal["quotedMixed"]] = "quotedMixed"
-QUOTED_W_DOTS: Final[Literal["quoted.w.dots"]] = "quoted.w.dots"
+from tests.integration.test_utils.data_source_config.base import DataSourceTestConfig
 
 
 class TestColumnQuotedIdentifiers:
-    pass
+    # Column names
+    UNQUOTED_UPPER_COL: Final[Literal["UNQUOTED_UPPER_COL"]] = "UNQUOTED_UPPER_COL"
+    UNQUOTED_LOWER_COL: Final[Literal["unquoted_lower_col"]] = "unquoted_lower_col"
+    UNQUOTED_MIXED_CASE: Final[Literal["unquotedMixed"]] = "unquotedMixed"
+    QUOTED_UPPER_COL: Final[Literal["QUOTED_UPPER_COL"]] = "QUOTED_UPPER_COL"
+    QUOTED_LOWER_COL: Final[Literal["quoted_lower_col"]] = "quoted_lower_col"
+    QUOTED_MIXED_CASE: Final[Literal["quotedMixed"]] = "quotedMixed"
+
+    SUPPORTED_DATA_SOURCES: list[DataSourceTestConfig] = [
+        DatabricksDatasourceTestConfig(),
+        PostgreSQLDatasourceTestConfig(),
+        SnowflakeDatasourceTestConfig(),
+        SqliteDatasourceTestConfig(),
+    ]
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame({UNQUOTED_LOWER_COL: [1, 2, 3]}),
+    # )
+    # def test_unquoted_lower(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_LOWER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame({QUOTED_LOWER_COL: [1, 2, 3]}),
+    # )
+    # def test_quoted_lower(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=QUOTED_LOWER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_unquoted_upper(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_UPPER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_quoted_upper(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=QUOTED_UPPER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_unquoted_mixed(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_LOWER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_quoted_mixed(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_LOWER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_unquoted_with_dots(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_LOWER_COL))
+    #     assert result.success
+
+    # @parameterize_batch_for_data_sources(
+    #     data_source_configs=SUPPORTED_DATA_SOURCES,
+    #     data=pd.DataFrame(),
+    # )
+    # def test_quoted_with_dots(self, batch_for_datasource: Batch):
+    #     result = batch_for_datasource.validate(gxe.ExpectColumnToExist(column=UNQUOTED_LOWER_COL))
+    #     assert result.success
 
 
 class TestTableQuotedIdentifiers:
+    TEST_TABLE_NAME: Final[Literal["test_table"]] = "test_table"
+
+    DatabaseType: TypeAlias = Literal["databricks", "postgresql", "snowflake", "sqlite"]
+    TableNameCase: TypeAlias = Literal[
+        "quoted_lower",
+        "quoted_mixed",
+        "quoted_upper",
+        "unquoted_lower",
+        "unquoted_mixed",
+        "unquoted_upper",
+    ]
+
+    TABLE_NAME_MAPPING: Final[Mapping[DatabaseType, Mapping[TableNameCase, str]]] = {
+        "postgres": {
+            "unquoted_lower": TEST_TABLE_NAME.lower(),
+            "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
+            "unquoted_upper": TEST_TABLE_NAME.upper(),
+            "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
+            "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
+            "unquoted_mixed": TEST_TABLE_NAME.title(),
+        },
+        "databricks": {
+            "unquoted_lower": TEST_TABLE_NAME.lower(),
+            "quoted_lower": f"`{TEST_TABLE_NAME.lower()}`",
+            "unquoted_upper": TEST_TABLE_NAME.upper(),
+            "quoted_upper": f"`{TEST_TABLE_NAME.upper()}`",
+            "quoted_mixed": f"`{TEST_TABLE_NAME.title()}`",
+            "unquoted_mixed": TEST_TABLE_NAME.title(),
+        },
+        "snowflake": {
+            "unquoted_lower": TEST_TABLE_NAME.lower(),
+            "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
+            "unquoted_upper": TEST_TABLE_NAME.upper(),
+            "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
+            "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
+            "unquoted_mixed": TEST_TABLE_NAME.title(),
+        },
+        "sqlite": {
+            "unquoted_lower": TEST_TABLE_NAME.lower(),
+            "quoted_lower": f'"{TEST_TABLE_NAME.lower()}"',
+            "unquoted_upper": TEST_TABLE_NAME.upper(),
+            "quoted_upper": f'"{TEST_TABLE_NAME.upper()}"',
+            "quoted_mixed": f'"{TEST_TABLE_NAME.title()}"',
+            "unquoted_mixed": TEST_TABLE_NAME.title(),
+        },
+    }
+
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["unquoted_lower"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["unquoted_lower"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["unquoted_lower"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["unquoted_lower"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["unquoted_lower"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_unquoted_lower(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
 
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["quoted_lower"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["quoted_lower"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["quoted_lower"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["quoted_lower"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["quoted_lower"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_quoted_lower(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
 
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["unquoted_upper"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["unquoted_upper"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["unquoted_upper"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["unquoted_upper"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["unquoted_upper"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_unquoted_upper(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
 
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["quoted_upper"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["quoted_upper"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["quoted_upper"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["quoted_upper"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["quoted_upper"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_quoted_upper(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
 
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["unquoted_mixed"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["unquoted_mixed"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["unquoted_mixed"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["unquoted_mixed"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["unquoted_mixed"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_unquoted_mixed(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
 
     @parameterize_batch_for_data_sources(
         data_source_configs=[
-            ds_type(table_name=TABLE_NAME_MAPPING[ds_type.label]["quoted_mixed"])
-            for ds_type in SUPPORTED_DATA_SOURCE_TYPES
+            DatabricksDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["databricks"]["quoted_mixed"]
+            ),
+            PostgreSQLDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["postgres"]["quoted_mixed"]
+            ),
+            SnowflakeDatasourceTestConfig(
+                table_name=TABLE_NAME_MAPPING["snowflake"]["quoted_mixed"]
+            ),
+            SqliteDatasourceTestConfig(table_name=TABLE_NAME_MAPPING["sqlite"]["quoted_mixed"]),
         ],
-        data=pd.DataFrame(),
+        data=pd.DataFrame({"a": [1, 2, 3]}),
     )
     def test_quoted_mixed(self, batch_for_datasource: Batch):
-        pass
+        result = batch_for_datasource.validate(gxe.ExpectTableRowCountToEqual(value=3))
+        assert result.success
