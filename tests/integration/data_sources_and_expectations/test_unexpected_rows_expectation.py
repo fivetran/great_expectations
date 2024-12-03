06f70025700ba187732d4ff764ef4ad1643f8a8c
Farmer <nathan@greatexpectations.io>
2024-12-03 14:29:43 -0700

    Introduce parametrized unexpected_rows_query

--- tests/integration/data_sources_and_expectations/test_unexpected_rows_expectation.py
+++ tests/integration/data_sources_and_expectations/test_unexpected_rows_expectation.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from typing import Sequence
 
 import pandas as pd
+import pytest
 
 import great_expectations.expectations as gxe
 from tests.integration.conftest import parameterize_batch_for_data_sources
@@ -40,15 +41,32 @@ PARTITIONER_SUPPORTED_DATA_SOURCES: Sequence[DataSourceTestConfig] = [
     SqliteDatasourceTestConfig(),
 ]
 
+DATA = pd.DataFrame(
+    {
+        "created_at": [
+            datetime(year=2024, month=12, day=1, tzinfo=timezone.utc).date(),
+            datetime(year=2024, month=11, day=30, tzinfo=timezone.utc).date(),
+        ],
+        "a": [1, 2],
+    }
+)
+
+DATE_COLUMN = "created_at"
+
+SUCCESS_QUERIES = ["SELECT * FROM {batch} WHERE a > 2"]
+
+FAILURE_QUERIES = ["SELECT * FROM {batch}"]
+
 
 @parameterize_batch_for_data_sources(
     data_source_configs=ALL_SUPPORTED_DATA_SOURCES,
-    data=pd.DataFrame({"a": [1, 2]}),
+    data=DATA,
 )
+@pytest.mark.parametrize("unexpected_rows_query", SUCCESS_QUERIES)
 def test_unexpected_rows_expectation_batch_keyword_success(
     batch_for_datasource,
+    unexpected_rows_query,
 ) -> None:
-    unexpected_rows_query = "SELECT * FROM {batch} WHERE a > 2"
     expectation = gxe.UnexpectedRowsExpectation(
         description="Expect a to be less than 3", unexpected_rows_query=unexpected_rows_query
     )
@@ -59,12 +77,13 @@ def test_unexpected_rows_expectation_batch_keyword_success(
 
 @parameterize_batch_for_data_sources(
     data_source_configs=ALL_SUPPORTED_DATA_SOURCES,
-    data=pd.DataFrame({"a": [1, 2]}),
+    data=DATA,
 )
+@pytest.mark.parametrize("unexpected_rows_query", FAILURE_QUERIES)
 def test_unexpected_rows_expectation_batch_keyword_failure(
     batch_for_datasource,
+    unexpected_rows_query,
 ) -> None:
-    unexpected_rows_query = "SELECT * FROM {batch}"
     expectation = gxe.UnexpectedRowsExpectation(
         description="Expect table to be empty", unexpected_rows_query=unexpected_rows_query
     )
@@ -75,23 +94,16 @@ def test_unexpected_rows_expectation_batch_keyword_failure(
 
 @parameterize_batch_for_data_sources(
     data_source_configs=PARTITIONER_SUPPORTED_DATA_SOURCES,
-    data=pd.DataFrame(
-        {
-            "created_at": [
-                datetime(year=2024, month=12, day=1, tzinfo=timezone.utc).date(),
-                datetime(year=2024, month=11, day=30, tzinfo=timezone.utc).date(),
-            ],
-            "a": [1, 2],
-        }
-    ),
+    data=DATA,
 )
+@pytest.mark.parametrize("unexpected_rows_query", SUCCESS_QUERIES)
 def test_unexpected_rows_expectation_batch_keyword_partitioner_success(
     asset_for_datasource,
+    unexpected_rows_query,
 ) -> None:
     batch = asset_for_datasource.add_batch_definition_monthly(
-        name="monthly success", column="created_at"
+        name="monthly success", column=DATE_COLUMN
     ).get_batch()
-    unexpected_rows_query = "SELECT * FROM {batch} WHERE a > 2"
     expectation = gxe.UnexpectedRowsExpectation(
         description="Expect a to be less than 3", unexpected_rows_query=unexpected_rows_query
     )
@@ -102,23 +114,16 @@ def test_unexpected_rows_expectation_batch_keyword_partitioner_success(
 
 @parameterize_batch_for_data_sources(
     data_source_configs=PARTITIONER_SUPPORTED_DATA_SOURCES,
-    data=pd.DataFrame(
-        {
-            "created_at": [
-                datetime(year=2024, month=12, day=1, tzinfo=timezone.utc).date(),
-                datetime(year=2024, month=11, day=30, tzinfo=timezone.utc).date(),
-            ],
-            "a": [1, 2],
-        }
-    ),
+    data=DATA,
 )
+@pytest.mark.parametrize("unexpected_rows_query", FAILURE_QUERIES)
 def test_unexpected_rows_expectation_batch_keyword_partitioner_failure(
     asset_for_datasource,
+    unexpected_rows_query,
 ) -> None:
     batch = asset_for_datasource.add_batch_definition_monthly(
-        name="monthly failure", column="created_at"
+        name="monthly failure", column=DATE_COLUMN
     ).get_batch()
-    unexpected_rows_query = "SELECT * FROM {batch}"
     expectation = gxe.UnexpectedRowsExpectation(
         description="Expect table to be empty", unexpected_rows_query=unexpected_rows_query
     )
