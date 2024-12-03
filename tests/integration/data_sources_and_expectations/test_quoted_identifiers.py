84683308e3fc34ac624f852afb2196e21c75a54a
Chetan Kini <chetan@superconductive.com>
2024-12-03 13:42:27 -0500

    start tests

--- tests/integration/data_sources_and_expectations/test_quoted_identifiers.py
+++ tests/integration/data_sources_and_expectations/test_quoted_identifiers.py
@@ -1,1 +1,36 @@
 # TODO: Write tests
+
+
+from typing import Sequence, Type
+
+from tests.integration.test_utils.data_source_config import (
+    BigQueryDatasourceTestConfig,
+    DatabricksDatasourceTestConfig,
+    DataSourceTestConfig,
+    MSSQLDatasourceTestConfig,
+    MySQLDatasourceTestConfig,
+    PostgreSQLDatasourceTestConfig,
+    SnowflakeDatasourceTestConfig,
+    SqliteDatasourceTestConfig,
+)
+
+SUPPORTED_DATA_SOURCE_TYPES: Sequence[Type[DataSourceTestConfig]] = [
+    BigQueryDatasourceTestConfig,
+    DatabricksDatasourceTestConfig,
+    MSSQLDatasourceTestConfig,
+    MySQLDatasourceTestConfig,
+    PostgreSQLDatasourceTestConfig,
+    SnowflakeDatasourceTestConfig,
+    SqliteDatasourceTestConfig,
+]
+
+
+# [ds_type(table_name=...) for ds_type in SUPPORTED_DATA_SOURCE_TYPES]
+
+
+class TestColumnQuotedIdentifiers:
+    pass
+
+
+class TestTableQuotedIdentifiers:
+    pass
