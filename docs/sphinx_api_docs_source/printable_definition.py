8016dbb2a16b5ef2c1db79c52841fc9d160bbfdc
Chetan Kini <chetan@superconductive.com>
2024-12-11 09:46:56 -0500

    remove override

--- docs/sphinx_api_docs_source/printable_definition.py
+++ docs/sphinx_api_docs_source/printable_definition.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 import pathlib
 from dataclasses import dataclass
 
-from great_expectations.compatibility.typing_extensions import override
-
 
 @dataclass(frozen=True)
 class PrintableDefinition:
@@ -15,7 +13,6 @@ class PrintableDefinition:
         if not self.file.exists():
             raise FileNotFoundError(f"File {self.file} does not exist.")  # noqa: TRY003
 
-    @override
     def __str__(self) -> str:
         return f"File: {self.file} Name: {self.name}"
 
