12e58812de7f903e251e3aefc8f12a2253229990
Thu Pham <thu.pham@greatexpectations.io>
2024-12-11 18:21:56 -0800

    [MAINTANENCE] Minor code clean up and refactor around column type expectations

--- great_expectations/expectations/core/expect_column_values_to_be_in_type_list.py
+++ great_expectations/expectations/core/expect_column_values_to_be_in_type_list.py
@@ -14,7 +14,7 @@ from great_expectations.core.suite_parameters import (  # noqa: TCH001
     SuiteParameterDict,
 )
 from great_expectations.expectations.core.expect_column_values_to_be_of_type import (
-    _get_dialect_type_module,
+    _get_potential_sqlalchemy_types,
     _native_type_type_map,
 )
 from great_expectations.expectations.expectation import (
@@ -457,55 +457,16 @@ class ExpectColumnValuesToBeInTypeList(ColumnMapExpectation):
             "result": {"observed_value": actual_column_type.type.__name__},
         }
 
-    def _validate_sqlalchemy(  # noqa: C901 - too complex
+    def _validate_sqlalchemy(
         self, actual_column_type, expected_types_list, execution_engine
     ):
-        # Our goal is to be as explicit as possible. We will match the dialect
-        # if that is possible. If there is no dialect available, we *will*
-        # match against a top-level SqlAlchemy type.
-        #
-        # This is intended to be a conservative approach.
-        #
-        # In particular, we *exclude* types that would be valid under an ORM
-        # such as "float" for postgresql with this approach
-
         if expected_types_list is None:
             success = True
         else:
             types = []
-            type_module = _get_dialect_type_module(execution_engine=execution_engine)
             for type_ in expected_types_list:
-                try:
-                    if type_module.__name__ == "pyathena.sqlalchemy_athena":
-                        potential_type = get_pyathena_potential_type(type_module, type_)
-                        # In the case of the PyAthena dialect we need to verify that
-                        # the type returned is indeed a type and not an instance.
-                        if not inspect.isclass(potential_type):
-                            real_type = type(potential_type)
-                        else:
-                            real_type = potential_type
-                        types.append(real_type)
-                    elif type_module.__name__ == "trino.sqlalchemy.datatype":
-                        potential_type = get_trino_potential_type(type_module, type_)
-                        types.append(type(potential_type))
-                    elif type_module.__name__ == "clickhouse_sqlalchemy.drivers.base":
-                        actual_column_type = get_clickhouse_sqlalchemy_potential_type(
-                            type_module, actual_column_type
-                        )()
-                        potential_type = get_clickhouse_sqlalchemy_potential_type(
-                            type_module, type_
-                        )
-                        types.append(potential_type)
-                    else:
-                        potential_type = getattr(type_module, type_)
-                        types.append(potential_type)
-                except AttributeError:
-                    logger.debug(f"Unrecognized type: {type_}")
-
-            if len(types) == 0:
-                logger.warning("No recognized sqlalchemy types in type_list for current dialect.")
-            types = tuple(types)
-            success = isinstance(actual_column_type, types)
+                types.append(_get_potential_sqlalchemy_types(type_, execution_engine))
+            success = isinstance(actual_column_type, tuple(types))
 
         return {
             "success": success,
