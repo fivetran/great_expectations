f9303ac4b52934a148a77d5695bdd49ee546cbbe
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2024-12-12 14:37:39 -0800

    code sample

---
+++ docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_custom_action.py
@@ -0,0 +1,41 @@
+"""
+This is an example script for how to create a custom Action.
+
+To test, run:
+pytest --docs-tests -k "docs_example_create_a_custom_action" tests/integration/test_script_runner.py
+"""
+
+# EXAMPLE SCRIPT STARTS HERE:
+
+# <snippet name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_checkpoint_with_actions.py - full code example">
+
+from typing import Literal
+from great_expectations.checkpoint.actions import ActionContext, ValidationAction
+from great_expectations.checkpoint.checkpoint import CheckpointResult
+
+# 1. Extend the ValidationAction class.
+# <snippet name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_checkpoint_with_actions.py - extend class">
+class MyCustomAction(ValidationAction):
+# </snippet>
+
+    # 2. Set the 'type' attribute to a unique string that identifies the action.
+    # <snippet name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_checkpoint_with_actions.py - set type">
+    type: Literal["my_custom_action"] = "my_custom_action"
+    # </snippet>
+
+    # 3. Override the run method to perform the action.
+    # <snippet name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_checkpoint_with_actions.py - override run">
+    @override
+    def run(
+        self, 
+        checkpoint_result: CheckpointResult, 
+        action_context: ActionContext, # Contains results from prior actions in the same checkpoint run.
+    ) -> dict:
+        self._do_my_custom_action(...) # Domain-specific logic
+        return {"some": "info"} # Return information about the action
+
+    def _do_my_custom_action(self):
+        ...
+    # </snippet>
+        
+# </snippet>
---
+++ docs/docusaurus/docs/core/trigger_actions_based_on_results/create_a_custom_action.md
@@ -0,0 +1,58 @@
+---
+title: Create a custom Action
+description: Run custom logic based on validation results to integrate with 3rd-party tools and business workflows.
+---
+import TabItem from '@theme/TabItem';
+import Tabs from '@theme/Tabs';
+
+import PrereqPythonInstalled from '../_core_components/prerequisites/_python_installation.md';
+import PrereqGxInstalled from '../_core_components/prerequisites/_gx_installation.md';
+
+TODO - intro
+
+<h2>Prerequisites</h2>
+
+- <PrereqPythonInstalled/>.
+- <PrereqGxInstalled/>.
+
+### Procedure
+
+<Tabs 
+   queryString="procedure"
+   defaultValue="instructions"
+   values={[
+      {value: 'instructions', label: 'Instructions'},
+      {value: 'sample_code', label: 'Sample code'}
+   ]}
+>
+
+1. Create a new custom Action class that inherits the ValidationAction class.
+
+   ```python title="Python" name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_custom_action.py - extend class" 
+   ```
+
+2. Choose a unique name for `type`
+
+   ```python title="Python" name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_custom_action.py - set type" 
+   ```
+
+3. Override the run method with the logic for the Action
+
+   ```python title="Python" name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_custom_action.py - override run" 
+   ```
+
+
+<TabItem value="instructions" label="Instructions">
+
+TODO instructions
+
+</TabItem>
+
+<TabItem value="sample_code" label="Sample code">
+
+```python title="Python" name="docs/docusaurus/docs/core/trigger_actions_based_on_results/_examples/create_a_custom_action.py - full code example" 
+```
+
+</TabItem>
+
+</Tabs>
