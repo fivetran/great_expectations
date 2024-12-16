6c89d7a170487d301da5362bc43dc4bc1226d92a
Farmer <nathan@greatexpectations.io>
2024-12-16 15:09:23 -0700

    Revert env variable overwrite change and update env var names

--- assets/docker/mercury/localstack_env_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# these are set by mercury localstack_init.sh during entrypoint
-# we re-set them after that script is run here
-export AWS_SECRET_ACCESS_KEY=$LOCALSTACK_AWS_SECRET_ACCESS_KEY
-export AWS_ACCESS_KEY_ID=$LOCALSTACK_AWS_ACCESS_KEY_ID
