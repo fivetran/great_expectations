35b93ec2d1e2c074c77240a798904a640be5303f
Farmer <nathan@greatexpectations.io>
2024-12-16 09:15:01 -0700

    Overwrite env vars that are set in localstack_init.sh

---
+++ assets/docker/mercury/localstack_env_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# these are set by mercury localstack_init.sh during entrypoint
+# we set them after that script is run here
+export AWS_SECRET_ACCESS_KEY=$LOCALSTACK_AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=$LOCALSTACK_AWS_ACCESS_KEY_ID
