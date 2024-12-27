3fa083e22660c16512aeedffaa44b2d72bb8c899
Deborah Niesz <deborah.niesz@10pines.com>
2024-12-27 12:56:45 -0300

    deleting redshift from

--- great_expectations/expectations/core/expect_column_quantile_values_to_be_between.py
+++ great_expectations/expectations/core/expect_column_quantile_values_to_be_between.py
@@ -76,7 +76,6 @@ SUPPORTED_DATA_SOURCES = [
     "PostgreSQL",
     "MySQL",
     "MSSQL",
-    "Redshift",
     "Snowflake",
     "BigQuery",
 ]
@@ -140,7 +139,6 @@ class ExpectColumnQuantileValuesToBeBetween(ColumnAggregateExpectation):
         [{SUPPORTED_DATA_SOURCES[5]}](https://docs.greatexpectations.io/docs/application_integration_support/)
         [{SUPPORTED_DATA_SOURCES[6]}](https://docs.greatexpectations.io/docs/application_integration_support/)
         [{SUPPORTED_DATA_SOURCES[7]}](https://docs.greatexpectations.io/docs/application_integration_support/)
-        [{SUPPORTED_DATA_SOURCES[8]}](https://docs.greatexpectations.io/docs/application_integration_support/)
 
     Data Quality Issues:
         {DATA_QUALITY_ISSUES[0]}
