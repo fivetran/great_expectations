077b1a331f578c47319a60b13a9f83f4aafee63a
Chetan Kini <chetan@superconductive.com>
2025-01-02 10:54:17 -0500

    Update great_expectations/datasource/fluent/sql_datasource.py
    Co-authored-by: Kristen Lavavej <35638702+klavavej@users.noreply.github.com>

--- great_expectations/datasource/fluent/sql_datasource.py
+++ great_expectations/datasource/fluent/sql_datasource.py
@@ -873,7 +873,7 @@ class _SQLAsset(DataAsset[DatasourceT, ColumnPartitioner], Generic[DatasourceT])
             name: The name of the batch definition to be added
             column: The column name on which to partition the asset by day
             sort_ascending: Boolean to indicate whether to sort ascending (default) or descending.
-                When running a validation, we default to running the last batch definition
+                When running a validation, we default to running the last Batch Definition
                 if one is not explicitly specified.
 
         Returns:
