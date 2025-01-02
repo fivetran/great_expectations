fd33f5a553b1be73a0ff16691a16ab6c04ff998f
Chetan Kini <chetan@superconductive.com>
2025-01-02 10:59:35 -0500

    Update great_expectations/datasource/fluent/sql_datasource.py
    Co-authored-by: Kristen Lavavej <35638702+klavavej@users.noreply.github.com>

--- great_expectations/datasource/fluent/sql_datasource.py
+++ great_expectations/datasource/fluent/sql_datasource.py
@@ -807,7 +807,7 @@ class _SQLAsset(DataAsset[DatasourceT, ColumnPartitioner], Generic[DatasourceT])
         sort_ascending: bool = True,
         validate_batchable: bool = True,
     ) -> BatchDefinition:
-        """Adds a yearly batch definition to this data asset
+        """Adds a yearly Batch Definition to this Data Asset
 
         Args:
             name: The name of the batch definition to be added
