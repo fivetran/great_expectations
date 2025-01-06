c20b02201ee931ab06efb09739aab5518f242283
Chetan Kini <chetan@superconductive.com>
2025-01-06 14:15:11 -0500

    update pin

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -252,7 +252,7 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev.txt", "adr-tools-python", (("==", "1.0.3"),)),
         ("requirements-dev.txt", "altair", (("<", "5.0.0"), (">=", "4.2.1"))),
         ("requirements-dev.txt", "docstring-parser", (("==", "0.16"),)),
-        ("requirements-dev.txt", "marshmallow", (("<", "4.0.0"), (">=", "3.7.1"))),
+        ("requirements-dev.txt", "marshmallow", (("<", "3.24.0"), (">=", "3.7.1"))),
         ("requirements-dev.txt", "moto", (("<", "5.0"), (">=", "4.2.13"))),
         ("requirements-dev.txt", "pandas", (("<", "2.2.0"),)),
         ("requirements-dev.txt", "posthog", (("<", "4"), (">", "3"))),
@@ -264,7 +264,7 @@ def test_polish_and_ratchet_pins_and_upper_bounds():
         ("requirements-dev.txt", "teradatasqlalchemy", (("==", "17.0.0.5"),)),
         ("requirements-dev.txt", "xlrd", (("<", "2.0.0"), (">=", "1.1.0"))),
         ("requirements.txt", "altair", (("<", "5.0.0"), (">=", "4.2.1"))),
-        ("requirements.txt", "marshmallow", (("<", "4.0.0"), (">=", "3.7.1"))),
+        ("requirements.txt", "marshmallow", (("<", "3.24.0"), (">=", "3.7.1"))),
         ("requirements.txt", "pandas", (("<", "2.2"),)),
         ("requirements.txt", "posthog", (("<", "4"), (">", "3"))),
     }
