8708117ae52b56710cb851dd6bc769e0c43b2a82
Farmer <nathan@greatexpectations.io>
2025-01-16 10:43:42 -0700

    Improve comment

--- great_expectations/data_context/store/tuple_store_backend.py
+++ great_expectations/data_context/store/tuple_store_backend.py
@@ -581,7 +581,8 @@ class TupleS3StoreBackend(TupleStoreBackend):
         data = s3_response_object["Body"].read()
         # Maybe it's theoretically possible to encode an entire file with multiple encodings.
         # It's an odd choice of format for ContentEncoding to allow multiple encoding strings.
-        # As far as we know, this list will always end up with length=1 though.
+        # As far as we know, this list will always end up with length=1 after the possible
+        # encoding `aws-chunked` is removed.
         for encoding in encodings:
             data = data.decode(encoding)
         return data
