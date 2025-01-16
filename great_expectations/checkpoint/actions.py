e50e648d5dcba5ce661a1e8e8d14cb82148afbf0
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-01-16 19:07:21 +0100

    Revert "move project manager into local import for actions"
    This reverts commit ce4bdec4cfa5d54368abf1a9b0f2e0cd4a48a1f8.

--- great_expectations/checkpoint/actions.py
+++ great_expectations/checkpoint/actions.py
@@ -42,6 +42,7 @@ from great_expectations.compatibility.pydantic import (
 from great_expectations.compatibility.pypd import pypd
 from great_expectations.compatibility.typing_extensions import override
 from great_expectations.data_context.cloud_constants import GXCloudRESTResource
+from great_expectations.data_context.data_context.context_factory import project_manager
 from great_expectations.data_context.types.resource_identifiers import (
     ExpectationSuiteIdentifier,
     GXCloudIdentifier,
@@ -209,8 +210,6 @@ class ValidationAction(BaseModel, metaclass=MetaValidationAction):
 
     @property
     def _using_cloud_context(self) -> bool:
-        from great_expectations.data_context.data_context.context_factory import project_manager
-
         return project_manager.is_using_cloud()
 
     @public_api
@@ -268,8 +267,6 @@ class DataDocsAction(ValidationAction):
         site_names: list[str] | None = None,
         resource_identifiers: list | None = None,
     ) -> dict:
-        from great_expectations.data_context.data_context.context_factory import project_manager
-
         return project_manager.build_data_docs(
             site_names=site_names, resource_identifiers=resource_identifiers
         )
@@ -279,8 +276,6 @@ class DataDocsAction(ValidationAction):
         site_names: list[str] | None = None,
         resource_identifier: Any | None = None,
     ):
-        from great_expectations.data_context.data_context.context_factory import project_manager
-
         return project_manager.get_docs_sites_urls(
             site_names=site_names, resource_identifier=resource_identifier
         )
