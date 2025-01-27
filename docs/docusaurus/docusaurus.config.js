4a8775b2ba5f7912e09d3e93210483e198950f5c
Deborah Niesz <deborah.niesz@10pines.com>
2025-01-27 14:58:37 -0300

    fix build error

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -191,13 +191,13 @@ module.exports = {
             {
               type: 'doc',
               label: 'Get support',
-              docId: 'get_support',
+              docId: 'docs/resources/get_support',
               className: 'non-versioned-section',
             },
             {
               type: 'doc',
               label: 'Integration support policy',
-              docId: 'application_integration_support',
+              docId: 'docs/resources/application_integration_support',
               className: 'non-versioned-section',
             },
             {
