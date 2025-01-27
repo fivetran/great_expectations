46823b2be7de6be03f5bb034e05fd0c243823068
Farmer <nathan@greatexpectations.io>
2025-01-27 12:28:55 -0700

    Combine SQL and Spark test cases

--- tests/integration/data_sources_and_expectations/test_expectation_conditions.py
+++ tests/integration/data_sources_and_expectations/test_expectation_conditions.py
@@ -126,6 +126,9 @@ PANDAS_TEST_CASES = [
                 "date_format": "mixed",
             },
         ),
+    ],
+    data=DATA,
+)
 @pytest.mark.parametrize(
     "row_condition",
     PANDAS_TEST_CASES,
@@ -144,7 +147,7 @@ def test_expect_column_min_to_be_between__pandas_row_condition(
     assert result.success
 
 
-SQL_TEST_CASES = [
+SQL_AND_SPARK_TEST_CASES = [
     pytest.param(
         'col("name")=="albert"',
         id="text-eq",
@@ -207,7 +210,7 @@ SQL_TEST_CASES = [
 )
 @pytest.mark.parametrize(
     "row_condition",
-    SQL_TEST_CASES,
+    SQL_AND_SPARK_TEST_CASES,
 )
 def test_expect_column_min_to_be_between__sql_row_condition(
     batch_for_datasource: Batch, row_condition: str
@@ -237,7 +240,7 @@ def test_expect_column_min_to_be_between__sql_row_condition(
 )
 @pytest.mark.parametrize(
     "row_condition",
-    SQL_TEST_CASES,
+    SQL_AND_SPARK_TEST_CASES,
 )
 def test_expect_column_min_to_be_between__snowflake_databricks_row_condition(
     batch_for_datasource: Batch, row_condition: str
@@ -253,125 +256,13 @@ def test_expect_column_min_to_be_between__snowflake_databricks_row_condition(
     assert result.success
 
 
-SPARK_TEST_CASES = [
-    pytest.param(
-        'col("name")=="albert"',
-        id="text-eq",
-    ),
-    pytest.param(
-        'col("quantity")<3',
-        id="number-lt",
-    ),
-    pytest.param(
-        'col("quantity")==1',
-        id="number-eq",
-    ),
-    pytest.param(
-        'col("updated_at")<date("2021-02-01"))',
-        id="date-lt",
-        marks=pytest.mark.xfail(
-            strict=True,
-            reason="Lack of support for date i/o in test framework",
-        ),
-    ),
-    pytest.param(
-        'col("updated_at")>date("2021-01-30"))',
-        id="date-gt",
-        marks=pytest.mark.xfail(
-            strict=True,
-            reason="Lack of support for date i/o in test framework",
-        ),
-    ),
-    pytest.param(
-        'col("updated_at")==date("2021-01-31"))',
-        id="date-eq",
-    ),
-    pytest.param(
-        'col("created_at")<date("2021-01-31 00:00:00"))',
-        id="datetime-lt",
-        marks=pytest.mark.xfail(
-            strict=True,
-            reason="Lack of support for date i/o in test framework",
-        ),
-    ),
-    pytest.param(
-        'col("created_at")>date("2021-01-29 00:00:00"))',
-        id="datetime-gt",
-        marks=pytest.mark.xfail(
-            strict=True,
-            reason="Lack of support for date i/o in test framework",
-        ),
-    ),
-]
-
-
-@parameterize_batch_for_data_sources(
-    data_source_configs=[
-        SparkFilesystemCsvDatasourceTestConfig(),
-    ],
-    data=DATA,
-)
-@pytest.mark.parametrize(
-    "row_condition",
-    SPARK_TEST_CASES,
-)
-def test_expect_column_min_to_be_between__spark_row_condition(
-    batch_for_datasource: Batch, row_condition: str
-) -> None:
-    expectation = gxe.ExpectColumnMinToBeBetween(
-        column="amount",
-        min_value=0.5,
-        max_value=1.5,
-        row_condition=row_condition,
-        condition_parser="great_expectations",
-    )
-    result = batch_for_datasource.validate(expectation)
-    assert result.success
-
-
-SPARK_TEST_CASES = [
-    pytest.param(
-        'col("name")=="albert"',
-        id="text-eq",
-    ),
-    pytest.param(
-        'col("quantity")<3',
-        id="number-lt",
-    ),
-    pytest.param(
-        'col("quantity")==1',
-        id="number-eq",
-    ),
-    pytest.param(
-        'col("updated_at")<date("2021-02-01"))',
-        id="date-lt",
-    ),
-    pytest.param(
-        'col("updated_at")>date("2021-01-30"))',
-        id="date-gt",
-    ),
-    pytest.param(
-        'col("updated_at")==date("2021-01-31"))',
-        id="date-eq",
-    ),
-    pytest.param(
-        'col("created_at")<date("2021-01-31 00:00:00"))',
-        id="datetime-lt",
-    ),
-    pytest.param(
-        'col("created_at")>date("2021-01-29 00:00:00"))',
-        id="datetime-gt",
-    ),
-]
-
-
 @parameterize_batch_for_data_sources(
     data_source_configs=[spark_filesystem_csv_datasource_test_config],
     data=DATA,
 )
 @pytest.mark.parametrize(
     "row_condition",
-    SPARK_TEST_CASES,
+    SQL_AND_SPARK_TEST_CASES,
 )
 def test_expect_column_min_to_be_between__spark_row_condition(
     batch_for_datasource: Batch, row_condition: str
