36c5566c692a9a7b06251fc49e9bcc069ef07e42
Farmer <nathan@greatexpectations.io>
2025-01-28 09:25:43 -0700

    Use json instead of orjson

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,7 @@
+import json
 from dataclasses import dataclass
 from typing import Callable, Generator, Mapping, Optional, Sequence, TypeVar
 
-import orjson
 import pandas as pd
 import pytest
 
@@ -30,7 +30,7 @@ class TestConfig:
         return hash(
             (
                 self.__class__,
-                orjson.dumps(self.data_source_config, option=orjson.OPT_SORT_KEYS),
+                json.dumps(self.data_source_config, sort_keys=True),
                 hash_data_frame(self.data),
                 dict_to_tuple(
                     {k: hash_data_frame(self.extra_data[k]) for k in sorted(self.extra_data)}
