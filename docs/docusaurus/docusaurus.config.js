6148f763977d74dc19d900f6b01bf6b27b52d41d
Deborah Niesz <deborah.niesz@10pines.com>
2025-02-13 18:06:02 -0300

    changed first option of algolia keys to be searched in Netlify's env variable value

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -75,8 +75,8 @@ module.exports = {
     },
     algolia: {
       // See: https://docusaurus.io/docs/search#connecting-algolia
-      appId: 'PFK639M3JK',
-      apiKey: '0d1a1d5150b166e798933d41988094c9',
+      appId: config.parsed.ALGOLIA_ID || 'PFK639M3JK',
+      apiKey: config.parsed.ALGOLIA_API_KEY || '0d1a1d5150b166e798933d41988094c9',
       indexName: 'greatexpectations',
       searchPagePath: 'search',
       contextualSearch: true,
