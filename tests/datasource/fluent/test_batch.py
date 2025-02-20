9e8d720485822541e325501b53e5ba006de3622d
Farmer <nathan@greatexpectations.io>
2025-02-20 00:02:35 -0700

    Update typing in test

--- tests/datasource/fluent/test_batch.py
+++ tests/datasource/fluent/test_batch.py
@@ -14,7 +14,7 @@ from great_expectations.expectations.expectation import Expectation
 from great_expectations.metrics import BatchRowCount, ColumnValuesBetween, Metric
 from great_expectations.metrics.batch.row_count import BatchRowCountResult
 from great_expectations.metrics.column_values.between import ColumnValuesBetweenResult
-from great_expectations.metrics.metric_results import MetricErrorResult, MetricResult
+from great_expectations.metrics.metric_results import MetricErrorResult
 
 DATASOURCE_NAME = "my_pandas"
 ASSET_NAME = "my_csv"
@@ -306,7 +306,7 @@ def test_batch_compute_metrics_single_metric_success(
         column="vendor_id",
         min_value=0,
     )
-    metric_results: list[MetricResult] | MetricResult = batch.compute_metrics(metric)
+    metric_results = batch.compute_metrics(metric)
     assert type(metric_results) is ColumnValuesBetweenResult
 
 
@@ -323,7 +323,7 @@ def test_batch_compute_metrics_multiple_metrics_success(
     metric_2 = BatchRowCount(batch_id=batch.id)
     metrics: list[Metric] = [metric_1, metric_2]
     requested_metric_count = len(metrics)
-    metric_results: list[MetricResult] | MetricResult = batch.compute_metrics(metrics)
+    metric_results = batch.compute_metrics(metrics)
     assert isinstance(metric_results, list)
     assert len(metric_results) == requested_metric_count
     assert type(metric_results[0]) is ColumnValuesBetweenResult
@@ -343,7 +343,7 @@ def test_batch_compute_metrics_multiple_metrics_error(
     metric_2 = BatchRowCount(batch_id=batch.id)
     metrics: list[Metric] = [metric_1, metric_2]
     requested_metric_count = len(metrics)
-    metric_results: list[MetricResult] | MetricResult = batch.compute_metrics(metrics)
+    metric_results = batch.compute_metrics(metrics)
     assert isinstance(metric_results, list)
     assert len(metric_results) == requested_metric_count
     assert type(metric_results[0]) is MetricErrorResult
