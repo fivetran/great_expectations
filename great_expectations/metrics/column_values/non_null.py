b89008460b2fd875c992811e8a12d586207da261
Farmer <nathan@greatexpectations.io>
2025-02-21 14:50:56 -0700

    Add an Enum for metric names

--- great_expectations/metrics/column_values/non_null.py
+++ great_expectations/metrics/column_values/non_null.py
@@ -1,5 +1,6 @@
 from great_expectations.metrics.domain import ColumnValues
 from great_expectations.metrics.metric import Metric
+from great_expectations.metrics.metric_names import MetricNames
 from great_expectations.metrics.metric_results import ConditionValues, MetricResult
 
 
@@ -7,11 +8,11 @@ class ColumnValuesNonNullResult(MetricResult[ConditionValues]): ...
 
 
 class ColumnValuesNonNull(Metric[ColumnValuesNonNullResult], ColumnValues):
-    name = "column_values.nonnull.condition"
+    name = MetricNames.COLUMN_VALUES_NON_NULL
 
 
 class ColumnValuesNonNullCountResult(MetricResult[int]): ...
 
 
 class ColumnValuesNonNullCount(Metric[ColumnValuesNonNullCountResult], ColumnValues):
-    name = "column_values.nonnull.count"
+    name = MetricNames.COLUMN_VALUES_NON_NULL_COUNT
