d9350bf361ce50841316bf31bb59cf80f4a7165e
Farmer <nathan@greatexpectations.io>
2025-02-21 13:14:04 -0700

    Expand data source coverage over ColumnValuesNonNull

--- tests/integration/metrics/column_values/test_non_null.py
+++ tests/integration/metrics/column_values/test_non_null.py
@@ -1,7 +1,6 @@
-from pathlib import Path
+from typing import Sequence
 
 import pandas as pd
-import pytest
 
 from great_expectations.compatibility.pyspark import functions as F
 from great_expectations.compatibility.sqlalchemy import sqlalchemy as sa
@@ -12,17 +11,19 @@ from great_expectations.metrics.column_values.non_null import (
     ColumnValuesNonNullCountResult,
     ColumnValuesNonNullResult,
 )
+from tests.integration.conftest import parameterize_batch_for_data_sources
 from tests.integration.test_utils.data_source_config import (
+    BigQueryDatasourceTestConfig,
+    DatabricksDatasourceTestConfig,
+    DataSourceTestConfig,
+    MSSQLDatasourceTestConfig,
+    MySQLDatasourceTestConfig,
     PandasDataFrameDatasourceTestConfig,
+    PandasFilesystemCsvDatasourceTestConfig,
     PostgreSQLDatasourceTestConfig,
+    SnowflakeDatasourceTestConfig,
     SparkFilesystemCsvDatasourceTestConfig,
-)
-from tests.integration.test_utils.data_source_config.pandas_data_frame import (
-    PandasDataFrameBatchTestSetup,
-)
-from tests.integration.test_utils.data_source_config.postgres import PostgresBatchTestSetup
-from tests.integration.test_utils.data_source_config.spark_filesystem_csv import (
-    SparkFilesystemCsvBatchTestSetup,
+    SqliteDatasourceTestConfig,
 )
 
 STRING_COLUMN_NAME = "letter"
@@ -41,107 +42,112 @@ try:
 except ModuleNotFoundError:
     SPARK_COLUMN_TYPES = {}
 
+PANDAS_DATA_SOURCES: Sequence[DataSourceTestConfig] = [
+    PandasFilesystemCsvDatasourceTestConfig(),
+    PandasDataFrameDatasourceTestConfig(),
+]
+
+SPARK_DATA_SOURCES: Sequence[DataSourceTestConfig] = [SparkFilesystemCsvDatasourceTestConfig()]
+
+SQL_DATA_SOURCES: Sequence[DataSourceTestConfig] = [
+    BigQueryDatasourceTestConfig(),
+    DatabricksDatasourceTestConfig(),
+    MSSQLDatasourceTestConfig(),
+    MySQLDatasourceTestConfig(),
+    PostgreSQLDatasourceTestConfig(),
+    SnowflakeDatasourceTestConfig(),
+    SqliteDatasourceTestConfig(),
+]
+
 
 class TestColumnValuesNonNull:
-    @pytest.mark.unit
-    def test_success_pandas(self) -> None:
-        batch_setup = PandasDataFrameBatchTestSetup(
-            config=PandasDataFrameDatasourceTestConfig(),
-            data=DATA_FRAME,
+    @parameterize_batch_for_data_sources(
+        data_source_configs=PANDAS_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_pandas(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullResult)
+        expected_value = (
+            pd.Series([False, True, False, False], name=STRING_COLUMN_NAME, dtype=bool),
+            IDDict({"batch_id": batch.id, "row_condition": None}),
+            {"column": STRING_COLUMN_NAME},
         )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullResult)
-            expected_value = (
-                pd.Series([False, True, False, False], name=STRING_COLUMN_NAME, dtype=bool),
-                IDDict({"batch_id": batch.id, "row_condition": None}),
-                {"column": STRING_COLUMN_NAME},
-            )
-            assert metric_result.value[0].equals(expected_value[0])
-            assert metric_result.value[1] == expected_value[1]
-            assert metric_result.value[2] == expected_value[2]
+        assert metric_result.value[0].equals(expected_value[0])
+        assert metric_result.value[1] == expected_value[1]
+        assert metric_result.value[2] == expected_value[2]
 
-    @pytest.mark.spark
-    def test_success_spark(self, tmp_path: Path) -> None:
-        batch_setup = SparkFilesystemCsvBatchTestSetup(
-            config=SparkFilesystemCsvDatasourceTestConfig(
-                column_types=SPARK_COLUMN_TYPES,
-            ),
-            data=DATA_FRAME,
-            base_dir=tmp_path,
+    @parameterize_batch_for_data_sources(
+        data_source_configs=SPARK_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_spark(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullResult)
+        expected_value = (
+            ~(F.col(STRING_COLUMN_NAME).isNotNull()),
+            IDDict({"batch_id": batch.id, "row_condition": None}),
+            {"column": STRING_COLUMN_NAME},
         )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullResult)
-            expected_value = (
-                ~(F.col(STRING_COLUMN_NAME).isNotNull()),
-                IDDict({"batch_id": batch.id, "row_condition": None}),
-                {"column": STRING_COLUMN_NAME},
-            )
-            assert str(metric_result.value[0]) == str(expected_value[0])
-            assert metric_result.value[1] == expected_value[1]
-            assert metric_result.value[2] == expected_value[2]
+        assert str(metric_result.value[0]) == str(expected_value[0])
+        assert metric_result.value[1] == expected_value[1]
+        assert metric_result.value[2] == expected_value[2]
 
-    @pytest.mark.postgresql
-    def test_success_postgres(self) -> None:
-        batch_setup = PostgresBatchTestSetup(
-            config=PostgreSQLDatasourceTestConfig(),
-            data=DATA_FRAME,
-            extra_data={},
+    @parameterize_batch_for_data_sources(
+        data_source_configs=SQL_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_sql(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullResult)
+        expected_value = (
+            sa.ColumnClause(STRING_COLUMN_NAME).is_(None),
+            IDDict({"batch_id": batch.id, "row_condition": None}),
+            {"column": STRING_COLUMN_NAME},
         )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNull(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullResult)
-            expected_value = (
-                sa.ColumnClause(STRING_COLUMN_NAME).is_(None),
-                IDDict({"batch_id": batch.id, "row_condition": None}),
-                {"column": STRING_COLUMN_NAME},
-            )
-            assert metric_result.value[0].compare(expected_value[0])
-            assert metric_result.value[1] == expected_value[1]
-            assert metric_result.value[2] == expected_value[2]
+        assert metric_result.value[0].compare(expected_value[0])
+        assert metric_result.value[1] == expected_value[1]
+        assert metric_result.value[2] == expected_value[2]
 
 
 class TestColumnValuesNonNullCount:
     NON_NULL_COUNT = 3
 
-    @pytest.mark.unit
-    def test_success_pandas(self) -> None:
-        batch_setup = PandasDataFrameBatchTestSetup(
-            config=PandasDataFrameDatasourceTestConfig(),
-            data=DATA_FRAME,
-        )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullCountResult)
-            assert metric_result.value == self.NON_NULL_COUNT
+    @parameterize_batch_for_data_sources(
+        data_source_configs=PANDAS_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_pandas(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullCountResult)
+        assert metric_result.value == self.NON_NULL_COUNT
 
-    @pytest.mark.spark
-    def test_success_spark(self, tmp_path: Path) -> None:
-        batch_setup = SparkFilesystemCsvBatchTestSetup(
-            config=SparkFilesystemCsvDatasourceTestConfig(),
-            data=DATA_FRAME,
-            base_dir=tmp_path,
-        )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullCountResult)
-            assert metric_result.value == self.NON_NULL_COUNT
+    @parameterize_batch_for_data_sources(
+        data_source_configs=SPARK_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_spark(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullCountResult)
+        assert metric_result.value == self.NON_NULL_COUNT
 
-    @pytest.mark.postgresql
-    def test_success_postgres(self) -> None:
-        batch_setup = PostgresBatchTestSetup(
-            config=PostgreSQLDatasourceTestConfig(),
-            data=DATA_FRAME,
-            extra_data={},
-        )
-        with batch_setup.batch_test_context() as batch:
-            metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
-            metric_result = batch.compute_metrics(metric)
-            assert isinstance(metric_result, ColumnValuesNonNullCountResult)
-            assert metric_result.value == self.NON_NULL_COUNT
+    @parameterize_batch_for_data_sources(
+        data_source_configs=SQL_DATA_SOURCES,
+        data=DATA_FRAME,
+    )
+    def test_success_sql(self, batch_for_datasource) -> None:
+        batch = batch_for_datasource
+        metric = ColumnValuesNonNullCount(batch_id=batch.id, column=STRING_COLUMN_NAME)
+        metric_result = batch.compute_metrics(metric)
+        assert isinstance(metric_result, ColumnValuesNonNullCountResult)
+        assert metric_result.value == self.NON_NULL_COUNT
