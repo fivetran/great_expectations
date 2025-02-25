ffdecd78f61689857db3d88be862f82c63e61901
Bill Dirks <bill@greatexpectations.io>
2025-02-25 13:19:41 -0800

    Add other datasources to test. Move test file.

---
+++ tests/integration/metrics/column_aggregate/test_column_aggregate.py
@@ -0,0 +1,69 @@
+from typing import Sequence
+
+import pandas
+
+from great_expectations.metrics.column_aggregate.mean import (
+    ColumnValuesMean,
+    ColumnValuesMeanResult,
+)
+from great_expectations.metrics.metric_results import MetricErrorResult
+from tests.integration.conftest import parameterize_batch_for_data_sources
+from tests.integration.test_utils.data_source_config import (
+    BigQueryDatasourceTestConfig,
+    DatabricksDatasourceTestConfig,
+    DataSourceTestConfig,
+    MSSQLDatasourceTestConfig,
+    PandasDataFrameDatasourceTestConfig,
+    PostgreSQLDatasourceTestConfig,
+    SnowflakeDatasourceTestConfig,
+    SparkFilesystemCsvDatasourceTestConfig,
+    SqliteDatasourceTestConfig,
+)
+
+DATA_FRAME = pandas.DataFrame(
+    {
+        "id": [1, 2, 3, 4],
+        "number": [1, 2, 3, 4],
+        "string": ["a", "b", "c", "d"],
+    },
+)
+
+DATA_SOURCES_WITHOUT_SPARK: Sequence[DataSourceTestConfig] = [
+    BigQueryDatasourceTestConfig(),
+    DatabricksDatasourceTestConfig(),
+    MSSQLDatasourceTestConfig(),
+    PostgreSQLDatasourceTestConfig(),
+    SnowflakeDatasourceTestConfig(),
+    SqliteDatasourceTestConfig(),
+    PandasDataFrameDatasourceTestConfig(),
+]
+
+DATA_SOURCES: Sequence[DataSourceTestConfig] = DATA_SOURCES_WITHOUT_SPARK + [
+    SparkFilesystemCsvDatasourceTestConfig()
+]
+
+
+@parameterize_batch_for_data_sources(
+    data_source_configs=DATA_SOURCES,
+    data=DATA_FRAME,
+)
+def test_mean_success(batch_for_datasource) -> None:
+    batch = batch_for_datasource
+    metric = ColumnValuesMean(batch_id=batch.id, column="number")
+    metric_result = batch.compute_metrics(metric)
+    assert isinstance(metric_result, ColumnValuesMeanResult)
+    assert metric_result.value == 2.5
+
+
+# For spark, when computing the mean, if it fails, the metric name changes from
+# `column.mean` to `column.aggregate.mean`.
+# There is a bug to track fixing this: https://greatexpectations.atlassian.net/browse/GX-448
+@parameterize_batch_for_data_sources(
+    data_source_configs=DATA_SOURCES_WITHOUT_SPARK,
+    data=DATA_FRAME,
+)
+def test_mean_failure(batch_for_datasource) -> None:
+    batch = batch_for_datasource
+    metric = ColumnValuesMean(batch_id=batch.id, column="string")
+    metric_result = batch.compute_metrics(metric)
+    assert isinstance(metric_result, MetricErrorResult)
