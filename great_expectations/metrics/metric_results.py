f5526ff7bc945c488e417700fddb880b6fffe93e
Farmer <nathan@greatexpectations.io>
2025-02-25 09:31:09 -0700

    Remove unused imports

--- great_expectations/metrics/metric_results.py
+++ great_expectations/metrics/metric_results.py
@@ -3,8 +3,6 @@ from typing import TYPE_CHECKING, Any, Generic, TypedDict, TypeVar, Union
 import pandas as pd
 
 from great_expectations.compatibility.pydantic import BaseModel, GenericModel
-from great_expectations.compatibility.pyspark import pyspark
-from great_expectations.compatibility.sqlalchemy import BinaryExpression
 from great_expectations.validator.exception_info import ExceptionInfo
 from great_expectations.validator.metric_configuration import (
     MetricConfiguration,
