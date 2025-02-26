dfc7a56129edd6cb660b50d04d7ca333bdeb22ae
Farmer <nathan@greatexpectations.io>
2025-02-26 15:41:34 -0700

    Add missing metric to metrics package

--- great_expectations/metrics/__init__.py
+++ great_expectations/metrics/__init__.py
@@ -2,3 +2,4 @@ from .batch.row_count import BatchRowCount
 from .column.mean import ColumnMean
 from .column.values_non_null import ColumnValuesNonNull, ColumnValuesNonNullCount
 from .metric import Metric
+from .query.row_count import QueryRowCount
