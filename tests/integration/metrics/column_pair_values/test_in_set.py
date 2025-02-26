ae60d09090f823b3ff1e0582476d1fd5e284d7dd
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-02-26 14:44:38 +0100

    add spark specific test

--- tests/integration/metrics/column_pair_values/test_in_set.py
+++ tests/integration/metrics/column_pair_values/test_in_set.py
@@ -7,6 +7,7 @@ from great_expectations.metrics.column_pair_values.in_set import (
     ColumnPairValuesInSetUnexpectedCountResult,
 )
 from tests.integration.conftest import parameterize_batch_for_data_sources
+from tests.integration.test_utils.data_source_config import SparkFilesystemCsvDatasourceTestConfig
 from tests.metrics.conftest import PANDAS_DATA_SOURCES, SPARK_DATA_SOURCES, SQL_DATA_SOURCES
 
 COL_A = "A"
@@ -77,9 +78,36 @@ class TestColumnPairValuesInSetUnexpectedValues:
         data_source_configs=PANDAS_DATA_SOURCES + SPARK_DATA_SOURCES,
         data=DATA_FRAME_WITH_NULLS,
     )
-    def test_ignore_row_if__pandas_and_spark(
+    def test_ignore_row_if__pandas(
         self, batch_for_datasource: Batch, ignore_row_if, unexpected_count
     ) -> None:
+        batch = batch_for_datasource
+        metric = ColumnPairValuesInSetUnexpectedCount(
+            batch_id=batch.id,
+            value_pairs_set=NO_MATCH_PAIR_SET,
+            column_A=COL_A_WITH_NULLS,
+            column_B=COL_B_WITH_NULLS,
+            ignore_row_if=ignore_row_if,
+        )
+        result = batch.compute_metrics(metric)
+        assert result.value == unexpected_count
+
+    @pytest.mark.parametrize(
+        "ignore_row_if,unexpected_count",
+        [
+            ("either_value_is_missing", 2),
+            ("both_values_are_missing", 6),
+            ("neither", 6),
+        ],
+    )
+    @parameterize_batch_for_data_sources(
+        data_source_configs=[SparkFilesystemCsvDatasourceTestConfig()],
+        data=DATA_FRAME_WITH_NULLS,
+    )
+    def test_ignore_row_if__spark(
+        self, batch_for_datasource: Batch, ignore_row_if, unexpected_count
+    ) -> None:
+        """This test captures unexpected behavior for Spark FileSystem data sources."""
         batch = batch_for_datasource
         metric = ColumnPairValuesInSetUnexpectedCount(
             batch_id=batch.id,
@@ -107,7 +135,7 @@ class TestColumnPairValuesInSetUnexpectedValues:
         self, batch_for_datasource: Batch, ignore_row_if, unexpected_count
     ) -> None:
         """This test captures a bug with SQL data sources and the ignore_row_if condition,
-        where SQL data sources always drop column pairs where both values are null.
+        where column pairs are dropped if both values are null.
         """
         batch = batch_for_datasource
         metric = ColumnPairValuesInSetUnexpectedCount(
