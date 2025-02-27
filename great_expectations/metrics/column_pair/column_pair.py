11e46723e66caa97f044b4d912cbcf8349027bf1
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-02-27 11:15:08 +0100

    add domain keys to metric base

--- great_expectations/metrics/column_pair/column_pair.py
+++ great_expectations/metrics/column_pair/column_pair.py
@@ -4,7 +4,7 @@ from great_expectations.metrics.batch import BatchMetric
 from great_expectations.metrics.metric import NonEmptyString, _MetricResult
 
 
-class ColumnPairMetric(BatchMetric[_MetricResult]):
+class ColumnPairMetric(BatchMetric[_MetricResult], kw_only=True):
     column_A: NonEmptyString
     column_B: NonEmptyString
     ignore_row_if: Literal["both_values_are_missing", "either_value_is_missing", "neither"] = (
