8e0d4bcf7ba2831bef885d80771b145482e55704
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-02-27 17:39:02 +0100

    reorg code

--- great_expectations/metrics/multi_column/sum_equal.py
+++ great_expectations/metrics/multi_column/sum_equal.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from great_expectations.metrics.metric_results import MetricResult
-from great_expectations.metrics.multi_column_values.multi_column_values import MultiColumnMetric
+from great_expectations.metrics.multi_column import MultiColumnMetric
 
 
 class MultiColumnSumEqualUnexpectedCountResult(MetricResult[int]): ...
