c0ce4a22ade56eda525a72cf68a753412503b910
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-02-27 17:48:45 +0100

    add import in __init__

--- great_expectations/metrics/__init__.py
+++ great_expectations/metrics/__init__.py
@@ -1,5 +1,6 @@
 from .batch.row_count import BatchRowCount
 from .column.mean import ColumnMean
 from .column.values_non_null import ColumnValuesNonNull, ColumnValuesNonNullCount
+from .multi_column.sum_equal import MultiColumnSumEqualUnexpectedCount
 from .metric import Metric
 from .query.row_count import QueryRowCount
