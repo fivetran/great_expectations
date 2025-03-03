4304d55c60a55b3895a891df3d7b8c0db0e8988b
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-03-03 11:45:41 -0500

    Fix another test

--- tests/analytics/test_analytics.py
+++ tests/analytics/test_analytics.py
@@ -164,6 +164,7 @@ def test_cloud_context_init(
         organization_id=UUID(cloud_details.org_id),
         oss_id=mock.ANY,
         cloud_mode=True,
+        mode="cloud",
         user_agent_str=user_agent_str,
     )
     mock_submit.assert_called_once_with(
