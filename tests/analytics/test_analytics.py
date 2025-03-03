d29ce4ad366c230d8778f9b848109d21dfb0b687
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-03-03 11:27:02 -0500

    Update some tests

--- tests/analytics/test_analytics.py
+++ tests/analytics/test_analytics.py
@@ -98,6 +98,7 @@ def test_ephemeral_context_init(monkeypatch):
         oss_id=mock.ANY,
         user_id=None,
         user_agent_str=None,
+        mode="ephemeral",
     )
     mock_submit.assert_called_once_with(
         mock.ANY,
@@ -221,6 +222,7 @@ def test_analytics_enabled_on_load(
         oss_id=mock.ANY,
         user_id=mock.ANY,
         user_agent_str=user_agent_str,
+        mode="ephemeral",
     )
 
 
@@ -249,6 +251,7 @@ def test_analytics_enabled_on_load__filesystem(
         oss_id=mock.ANY,
         user_id=mock.ANY,
         user_agent_str=user_agent_str,
+        mode="file",
     )
 
 
@@ -286,6 +289,7 @@ def test_analytics_enabled_after_setting_explicitly(
         oss_id=mock.ANY,
         user_id=mock.ANY,
         user_agent_str=mock.ANY,
+        mode="ephemeral",
     )
 
 
@@ -316,4 +320,5 @@ def test_user_agent_str_after_setting_explicitly(
         oss_id=mock.ANY,
         user_id=mock.ANY,
         user_agent_str=new_user_agent_str,
+        mode="ephemeral",
     )
