6672de973fb9f84cef58fd490e245ad7c0219859
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-03-07 11:16:21 -0500

    Stop using removed functions

--- docs/docusaurus/src/theme/DocCard/index.js
+++ docs/docusaurus/src/theme/DocCard/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
-import {
-  findFirstSidebarItemLink,
-  useDocById,
-} from '@docusaurus/theme-common/internal';
+import {useDocsSidebar} from '@docusaurus/theme-common/internal';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 import {translate} from '@docusaurus/Translate';
 import styles from './styles.module.css';
@@ -19,6 +16,7 @@ function CardContainer({href, children}) {
     </Link>
   );
 }
+
 function CardLayout({item, href, icon, title, description}) {
   return (
     <CardContainer href={href}>
@@ -35,8 +33,24 @@ function CardLayout({item, href, icon, title, description}) {
     </CardContainer>
   );
 }
+
+function findFirstLink(item) {
+  if (item.href) {
+    return item.href;
+  }
+  if (item.items?.length) {
+    for (const subItem of item.items) {
+      const link = findFirstLink(subItem);
+      if (link) {
+        return link;
+      }
+    }
+  }
+  return null;
+}
+
 function CardCategory({item}) {
-  const href = findFirstSidebarItemLink(item);
+  const href = findFirstLink(item);
   // Unexpected: categories that don't have a link have been filtered upfront
   if (!href) {
     return null;
@@ -44,7 +58,7 @@ function CardCategory({item}) {
   return (
     <CardLayout
       href={href}
-      icon=<img src={useBaseUrl(`img/groupicon.svg`)} alt="icon" />
+      icon={<img src={useBaseUrl(`img/groupicon.svg`)} alt="icon" />}
       title={item.label}
       description={translate(
         {
@@ -58,9 +72,11 @@ function CardCategory({item}) {
     />
   );
 }
+
 function CardLink({item}) {
   const icon = isInternalUrl(item.href) ? <img src={useBaseUrl(`img/integrations/page_icon.svg`)} alt="icon" /> : '🔗';
-  const doc = useDocById(item.docId ?? undefined);
+  const sidebar = useDocsSidebar();
+  const doc = sidebar?.items.find(docItem => docItem.docId === item.docId);
   return (
     <CardLayout
       href={item.href}
@@ -71,6 +87,7 @@ function CardLink({item}) {
     />
   );
 }
+
 export default function DocCard({item}) {
   switch (item.type) {
     case 'link':
