fe3518abc286464da77b005109288b3611c5ebaa
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-04-07 13:45:12 -0400

    Backout unneeded changes

--- tests/metrics/conftest.py
+++ tests/metrics/conftest.py
@@ -26,7 +26,3 @@ SQL_DATA_SOURCES: list[DataSourceTestConfig] = [
     SnowflakeDatasourceTestConfig(),
     SqliteDatasourceTestConfig(),
 ]
-
-ALL_DATA_SOURCES: list[DataSourceTestConfig] = (
-    PANDAS_DATA_SOURCES + SPARK_DATA_SOURCES + SQL_DATA_SOURCES
-)
