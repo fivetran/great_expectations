26cdf085532b6990bc6f543b5051f4c33d39c413
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-04-08 16:04:09 -0400

    Update tests

--- tests/integration/metrics/column/test_column_distinct_values_count.py
+++ tests/integration/metrics/column/test_column_distinct_values_count.py
@@ -6,41 +6,19 @@ from great_expectations.metrics.column.column_distinct_values_count import (
     ColumnDistinctValuesCountResult,
 )
 from tests.integration.conftest import parameterize_batch_for_data_sources
-from tests.metrics.conftest import ALL_DATA_SOURCES, SparkFilesystemCsvDatasourceTestConfig
+from tests.metrics.conftest import ALL_DATA_SOURCES
 
 COLUMN_NAME = "whatevs"
-NULL_COLUMN_NAME = "nulls"
 DATA_FRAME = pd.DataFrame(
     {
-        COLUMN_NAME: ["a", "b", "c", "c", "c", "c", "c", "c", "c", "c"],
-        NULL_COLUMN_NAME: [None, None, None, None, None, None, None, None, None, None],
+        COLUMN_NAME: ["a", "b", "c", "c", "c", "c", "c", "c", "c", "c", None, None],
     },
 )
 
-try:
-    from great_expectations.compatibility.pyspark import types as PYSPARK_TYPES
 
-    SPARK_COLUMN_TYPES = {
-        COLUMN_NAME: PYSPARK_TYPES.StringType,
-        NULL_COLUMN_NAME: PYSPARK_TYPES.StringType,
-    }
-except ModuleNotFoundError:
-    SPARK_COLUMN_TYPES = {}
-
-
-ALL_DATA_SOURCES_EXCEPT_SPARK = [
-    datasource
-    for datasource in ALL_DATA_SOURCES
-    if not isinstance(datasource, SparkFilesystemCsvDatasourceTestConfig)
-]
-
-
-class TestColumnNullCount:
+class TestColumnDistinctValuesCount:
     @parameterize_batch_for_data_sources(
-        data_source_configs=[
-            *ALL_DATA_SOURCES_EXCEPT_SPARK,
-            SparkFilesystemCsvDatasourceTestConfig(column_types=SPARK_COLUMN_TYPES),
-        ],
+        data_source_configs=ALL_DATA_SOURCES,
         data=DATA_FRAME,
     )
     def test_strings(self, batch_for_datasource: Batch) -> None:
@@ -49,14 +27,3 @@ class TestColumnNullCount:
 
         assert isinstance(metric_result, ColumnDistinctValuesCountResult)
         assert metric_result.value == 3
-
-    @parameterize_batch_for_data_sources(
-        data_source_configs=ALL_DATA_SOURCES,
-        data=DATA_FRAME,
-    )
-    def test_all_nulls(self, batch_for_datasource: Batch) -> None:
-        metric = ColumnDistinctValuesCount(column=NULL_COLUMN_NAME)
-        metric_result = batch_for_datasource.compute_metrics(metric)
-
-        assert isinstance(metric_result, ColumnDistinctValuesCountResult)
-        assert metric_result.value == 0
