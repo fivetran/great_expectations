9142dce2149fb82f42eab5ab27d109fad53871af
Galina Yermicheva <galina.yermicheva@greatexpectations.io>
2025-04-09 16:33:37 -0400

    Small formatting

--- tests/expectations/test_expectation.py
+++ tests/expectations/test_expectation.py
@@ -588,10 +588,7 @@ class TestCustomAnnotatedFields:
         [
             ({"1", "2", "3"}, list),
             (("1", "2", "3"), list),
-            (
-                ["1", "2", "3"],
-                list,
-            ),
+            (["1", "2", "3"], list),
             ({"$PARAMETER": "my_param"}, dict),
         ],
     )
