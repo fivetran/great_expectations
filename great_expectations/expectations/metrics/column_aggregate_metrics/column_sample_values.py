31bb360386bff4a083474ed442ffdef390a7ca69
Farmer <nathan@greatexpectations.io>
2025-04-09 16:47:04 -0600

    Fix metric provider

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_sample_values.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_sample_values.py
@@ -1,18 +1,31 @@
 from __future__ import annotations
 
 from great_expectations.compatibility.sqlalchemy import sqlalchemy as sa
+from great_expectations.core.metric_domain_types import MetricDomainTypes
 from great_expectations.execution_engine import (
     SqlAlchemyExecutionEngine,
 )
 from great_expectations.expectations.metrics.column_aggregate_metric_provider import (
     ColumnAggregateMetricProvider,
-    column_aggregate_partial,
 )
+from great_expectations.expectations.metrics.metric_provider import metric_value
 
 
 class ColumnSampleValues(ColumnAggregateMetricProvider):
     metric_name = "column.sample_values"
 
-    @column_aggregate_partial(engine=SqlAlchemyExecutionEngine)
-    def _sqlalchemy(cls, column, count, **kwargs):
-        return sa.func.sample(column, count)
+    @metric_value(engine=SqlAlchemyExecutionEngine)
+    def _sqlalchemy(
+        cls,
+        execution_engine: SqlAlchemyExecutionEngine,
+        metric_domain_kwargs: dict,
+        metric_value_kwargs: dict,
+        **kwargs,
+    ):
+        selectable, _, _ = execution_engine.get_compute_domain(
+            domain_kwargs=metric_domain_kwargs, domain_type=MetricDomainTypes.COLUMN
+        )
+        column = metric_domain_kwargs["column"]
+        count = metric_value_kwargs["count"]
+        query: sa.Select = sa.select(sa.column(column)).select_from(selectable).limit(count)  # type: ignore[arg-type] # FIXME CoP
+        return execution_engine.execute_query(query).fetchall()
