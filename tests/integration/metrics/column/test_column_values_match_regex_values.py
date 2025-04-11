6416c30627231ce26a2118f87832ac4784338b08
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-04-11 09:11:59 -0400

    Handle snowflake apparently not handling partial matches

--- tests/integration/metrics/column/test_column_values_match_regex_values.py
+++ tests/integration/metrics/column/test_column_values_match_regex_values.py
@@ -6,7 +6,7 @@ from great_expectations.metrics.column.column_values_match_regex_values import (
     ColumnValuesMatchRegexValuesResult,
 )
 from tests.integration.conftest import parameterize_batch_for_data_sources
-from tests.metrics.conftest import SQL_DATA_SOURCES
+from tests.metrics.conftest import SQL_DATA_SOURCES, SnowflakeDatasourceTestConfig
 
 COLUMN_NAME = "whatevs"
 BIG_NUMBER = 101
@@ -15,13 +15,19 @@ MATCH_ALL_REGEX = ".+"
 DATA_FRAME = pd.DataFrame({COLUMN_NAME: ["abc", "def", "ghi", "1ab2", None]})
 DATA_FRAME_WITH_LOTS_OF_VALUES = pd.DataFrame({COLUMN_NAME: ["A"] * BIG_NUMBER})
 
+SQL_DATA_SOURCES_EXCEPT_SNOWFLAKE = [
+    datasource
+    for datasource in SQL_DATA_SOURCES
+    if not isinstance(datasource, SnowflakeDatasourceTestConfig)
+]
+
 
 class TestColumnValuesMatchRegexValues:
     @parameterize_batch_for_data_sources(
-        data_source_configs=SQL_DATA_SOURCES,
+        data_source_configs=SQL_DATA_SOURCES_EXCEPT_SNOWFLAKE,
         data=DATA_FRAME,
     )
-    def test_regular_characters(self, batch_for_datasource: Batch) -> None:
+    def test_partial_match_characters(self, batch_for_datasource: Batch) -> None:
         metric = ColumnValuesMatchRegexValues(column=COLUMN_NAME, regex="ab")
         metric_result = batch_for_datasource.compute_metrics(metric)
 
