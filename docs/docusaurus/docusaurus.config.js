e69f8d39121d40aaab220f886efce8b3dd892b6d
Aaron Ross <aaron.ross@greatexpectations.io>
2025-04-14 09:02:48 -0700

    keep both .18 versions

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -318,7 +318,10 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.3.14'
+              label: '1.3.14',
+            },
+            ['0.18']: {
+              label: '0.18.21',
             },
             0.18: {
               label: '0.18.21'
