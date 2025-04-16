a5ec1c587bc1d7aae6ec0f8d2cf2e55efdae0c9e
Alena Hutchinson <alena.hutchinson@greatexpectations.io>
2025-04-16 11:51:36 -0700

    remove configdict and allow for coersion

--- great_expectations/datasource/fluent/redshift_datasource.py
+++ great_expectations/datasource/fluent/redshift_datasource.py
@@ -43,7 +43,6 @@ class RedshiftConnectionDetails(BaseModel):
     Alternative to a connection string.
     """
 
-    model_config = ConfigDict(strict=True)
     username: StrictStr
     password: Union[ConfigStr, StrictStr]
     host: StrictStr
