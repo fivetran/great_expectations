48d4a5cbe2938b50d1a3c8c8c1e20fcac7c7bcc1
Farmer <nathan@greatexpectations.io>
2025-04-23 13:31:18 -0600

    Try all data sources

--- tests/integration/metrics/test_error_result.py
+++ tests/integration/metrics/test_error_result.py
@@ -3,17 +3,7 @@ import pandas
 from great_expectations.metrics.column.mean import ColumnMean
 from great_expectations.metrics.metric_results import MetricErrorResult
 from tests.integration.conftest import parameterize_batch_for_data_sources
-from tests.integration.test_utils.data_source_config import (
-    BigQueryDatasourceTestConfig,
-    DatabricksDatasourceTestConfig,
-    DataSourceTestConfig,
-    MSSQLDatasourceTestConfig,
-    PandasDataFrameDatasourceTestConfig,
-    PostgreSQLDatasourceTestConfig,
-    SnowflakeDatasourceTestConfig,
-    SparkFilesystemCsvDatasourceTestConfig,
-    SqliteDatasourceTestConfig,
-)
+from tests.integration.test_utils.data_source_config import ALL_DATA_SOURCES
 
 DATA_FRAME = pandas.DataFrame(
     {
@@ -22,20 +12,9 @@ DATA_FRAME = pandas.DataFrame(
     },
 )
 
-DATA_SOURCES: list[DataSourceTestConfig] = [
-    BigQueryDatasourceTestConfig(),
-    MSSQLDatasourceTestConfig(),
-    PostgreSQLDatasourceTestConfig(),
-    SnowflakeDatasourceTestConfig(),
-    PandasDataFrameDatasourceTestConfig(),
-    SparkFilesystemCsvDatasourceTestConfig(),
-    DatabricksDatasourceTestConfig(),
-    SqliteDatasourceTestConfig(),
-]
-
 
 @parameterize_batch_for_data_sources(
-    data_source_configs=DATA_SOURCES,
+    data_source_configs=ALL_DATA_SOURCES,
     data=DATA_FRAME,
 )
 def test_error_result(batch_for_datasource) -> None:
