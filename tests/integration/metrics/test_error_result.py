2a4a8e0de2a73d8a59a7ef7f9436f28913d85b00
Farmer <nathan@greatexpectations.io>
2025-04-23 13:52:39 -0600

    Use different metric in error test

--- tests/integration/metrics/test_error_result.py
+++ tests/integration/metrics/test_error_result.py
@@ -1,6 +1,6 @@
 import pandas
 
-from great_expectations.metrics.column.mean import ColumnMean
+from great_expectations.metrics.column.distinct_values import ColumnDistinctValues
 from great_expectations.metrics.metric_results import MetricErrorResult
 from tests.integration.conftest import parameterize_batch_for_data_sources
 from tests.metrics.conftest import ALL_DATA_SOURCES
@@ -19,7 +19,7 @@ DATA_FRAME = pandas.DataFrame(
 )
 def test_error_result(batch_for_datasource) -> None:
     batch = batch_for_datasource
-    metric = ColumnMean(column="non_existent_column")
+    metric = ColumnDistinctValues(column="non_existent_column")
     metric_result = batch.compute_metrics(metric)
     assert isinstance(metric_result, MetricErrorResult)
     assert metric_result.value.exception_message is not None
