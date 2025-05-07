f1e0cfe730d32ad751a97f7d3391909489c9f63b
Farmer <nathan@greatexpectations.io>
2025-05-07 09:55:57 -0600

    Get rid of test case constants

--- tests/integration/data_sources_and_expectations/expectations/test_expect_query_results_to_match_source.py
+++ tests/integration/data_sources_and_expectations/expectations/test_expect_query_results_to_match_source.py
@@ -19,38 +19,35 @@ SOURCE_DATA_WITH_DUPS = pd.DataFrame({"a": [1, 1, 2, 3], "b": [4, 4, 5, 6]})
 TARGET_DATA_WITH_DUPS = pd.DataFrame({"a": [1, 1, 2, 3], "b": [4, 4, 5, 6]})
 
 
-SUCCESS_TEST_CASES = [
-    pytest.param(
-        "SELECT a, b FROM {batch} ORDER BY a, b",
-        "SELECT a, b FROM {source_table} ORDER BY a, b",
-        id="multiple_columns_multiple_rows",
-    ),
-    pytest.param(
-        "SELECT a FROM {batch} ORDER BY a",
-        "SELECT a FROM {source_table} ORDER BY a",
-        id="one_column_multiple_rows",
-    ),
-    pytest.param(
-        "SELECT a, b FROM {batch} ORDER BY b LIMIT 1",
-        "SELECT a, b FROM {source_table} ORDER BY b LIMIT 1",
-        id="multiple_columns_one_row",
-    ),
-    pytest.param(
-        "SELECT a, b FROM {batch} LIMIT 0",
-        "SELECT a, b FROM {source_table} LIMIT 0",
-        id="both_results_are_empty",
-    ),
-    pytest.param(
-        "SELECT a, c FROM {batch} ORDER BY c",
-        "SELECT a, b FROM {source_table} ORDER BY b",
-        id="column_names_different_values_the_same",
-    ),
-]
-
-
 @pytest.mark.parametrize(
     "target_query,source_query",
-    SUCCESS_TEST_CASES,
+    [
+        pytest.param(
+            "SELECT a, b FROM {batch} ORDER BY a, b",
+            "SELECT a, b FROM {source_table} ORDER BY a, b",
+            id="multiple_columns_multiple_rows",
+        ),
+        pytest.param(
+            "SELECT a FROM {batch} ORDER BY a",
+            "SELECT a FROM {source_table} ORDER BY a",
+            id="one_column_multiple_rows",
+        ),
+        pytest.param(
+            "SELECT a, b FROM {batch} ORDER BY b LIMIT 1",
+            "SELECT a, b FROM {source_table} ORDER BY b LIMIT 1",
+            id="multiple_columns_one_row",
+        ),
+        pytest.param(
+            "SELECT a, b FROM {batch} LIMIT 0",
+            "SELECT a, b FROM {source_table} LIMIT 0",
+            id="both_results_are_empty",
+        ),
+        pytest.param(
+            "SELECT a, c FROM {batch} ORDER BY c",
+            "SELECT a, b FROM {source_table} ORDER BY b",
+            id="column_names_different_values_the_same",
+        ),
+    ],
 )
 @multi_source_batch_setup(
     multi_source_test_configs=ALL_SOURCE_TO_TARGET_SOURCES,
@@ -72,31 +69,30 @@ def test_expect_query_results_to_match_source_success(
     assert result.success
 
 
-FAILURE_TEST_CASES = [
-    pytest.param(
-        "SELECT * FROM {batch}", "SELECT * FROM {source_table}", id="duplicate_values_across_rows"
-    ),
-    pytest.param(
-        "SELECT a, b FROM {batch} LIMIT 2",
-        "SELECT a, b FROM {source_table}",
-        id="row_count_mismatch",
-    ),
-    pytest.param(
-        "SELECT a FROM {batch} ORDER BY a",
-        "SELECT b FROM {source_table} ORDER BY a",
-        id="column_value_mismatch",
-    ),
-    pytest.param(
-        "SELECT * FROM {batch} LIMIT 0",
-        "SELECT * FROM {source_table} ORDER BY a",
-        id="one_result_is_empty",
-    ),
-]
-
-
 @pytest.mark.parametrize(
     "target_query,source_query",
-    FAILURE_TEST_CASES,
+    [
+        pytest.param(
+            "SELECT * FROM {batch}",
+            "SELECT * FROM {source_table}",
+            id="duplicate_values_across_rows",
+        ),
+        pytest.param(
+            "SELECT a, b FROM {batch} LIMIT 2",
+            "SELECT a, b FROM {source_table}",
+            id="row_count_mismatch",
+        ),
+        pytest.param(
+            "SELECT a FROM {batch} ORDER BY a",
+            "SELECT b FROM {source_table} ORDER BY a",
+            id="column_value_mismatch",
+        ),
+        pytest.param(
+            "SELECT * FROM {batch} LIMIT 0",
+            "SELECT * FROM {source_table} ORDER BY a",
+            id="one_result_is_empty",
+        ),
+    ],
 )
 @multi_source_batch_setup(
     multi_source_test_configs=ALL_SOURCE_TO_TARGET_SOURCES,
@@ -119,15 +115,12 @@ def test_expect_query_results_to_match_source_failure(
     assert not result.exception_info["raised_exception"]
 
 
-MOSTLY_TEST_CASES = [
-    pytest.param(0.9, False, id="mostly_failure"),
-    pytest.param(0.5, True, id="mostly_success"),
-]
-
-
 @pytest.mark.parametrize(
     "mostly,success",
-    MOSTLY_TEST_CASES,
+    [
+        pytest.param(0.9, False, id="mostly_failure"),
+        pytest.param(0.5, True, id="mostly_success"),
+    ],
 )
 @multi_source_batch_setup(
     multi_source_test_configs=ALL_SOURCE_TO_TARGET_SOURCES,
