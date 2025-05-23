dbb86fc1e71971052290400a2f5a4fa0d7b927da
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2025-05-23 10:27:34 -0400

    Fix an unrelated type?

--- great_expectations/execution_engine/sparkdf_execution_engine.py
+++ great_expectations/execution_engine/sparkdf_execution_engine.py
@@ -271,7 +271,7 @@ class SparkDFExecutionEngine(ExecutionEngine):
 
         spark_session: pyspark.SparkSession
         try:
-            spark_session = pyspark.SparkConnectSession.builder.getOrCreate()
+            spark_session = pyspark.SparkConnectSession.builder.getOrCreate()  # type: ignore [assignment]
         except (ModuleNotFoundError, ValueError, KeyError):
             spark_session = pyspark.SparkSession.builder.getOrCreate()
 
