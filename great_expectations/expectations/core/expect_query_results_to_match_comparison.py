b1ebeb21a918895ac96e7313f7809c33a6f714b6
Farmer <nathan@greatexpectations.io>
2025-05-27 12:30:05 -0600

    Handle rendering edge cases

--- great_expectations/expectations/core/expect_query_results_to_match_comparison.py
+++ great_expectations/expectations/core/expect_query_results_to_match_comparison.py
@@ -2,7 +2,17 @@ from __future__ import annotations
 
 from collections import Counter
 from functools import cmp_to_key
-from typing import TYPE_CHECKING, Any, ClassVar, Dict, Literal, Optional, Tuple, Type, Union
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    ClassVar,
+    Dict,
+    Literal,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+)
 
 from great_expectations.compatibility import pydantic
 from great_expectations.compatibility.typing_extensions import override
@@ -322,7 +332,9 @@ class ExpectQueryResultsToMatchComparison(BatchExpectation):
         missing_rows_table = cls._create_observed_values_table(missing_rows)
         unexpected_rows_table = cls._create_observed_values_table(unexpected_rows)
 
-        if (
+        if len(missing_rows) == 0 and len(unexpected_rows) == 0:
+            return []
+        elif (
             len(missing_rows) == 1
             and len(unexpected_rows) == 1
             and len(missing_rows_cols) == 1
@@ -335,13 +347,13 @@ class ExpectQueryResultsToMatchComparison(BatchExpectation):
                 result=result,
                 runtime_configuration=runtime_configuration,
             )
-        elif len(missing_rows_cols) == 1 and len(unexpected_rows_cols) == 1:
+        elif len(missing_rows_cols) <= 1 and len(unexpected_rows_cols) <= 1:
             return cls._create_observed_values_set(
+                comparison_col_name=missing_rows_cols[0] if missing_rows_cols else None,
+                base_col_name=unexpected_rows_cols[0] if unexpected_rows_cols else None,
                 configuration=configuration,
                 result=result,
                 runtime_configuration=runtime_configuration,
-                comparison_col_name=missing_rows_cols[0],
-                base_col_name=unexpected_rows_cols[0],
             )
         else:
             return [
@@ -414,15 +426,23 @@ class ExpectQueryResultsToMatchComparison(BatchExpectation):
     @classmethod
     def _create_observed_values_set(
         cls,
-        comparison_col_name: str,
-        base_col_name: str,
+        comparison_col_name: Optional[str] = None,
+        base_col_name: Optional[str] = None,
         configuration: Optional[ExpectationConfiguration] = None,
         result: Optional[ExpectationValidationResult] = None,
         runtime_configuration: Optional[dict] = None,
     ) -> RenderedAtomicContent:
         result_details = cls._get_details_from_results(result)
-        comparison_values = [row[comparison_col_name] for row in result_details["missing_rows"]]
-        base_values = [row[base_col_name] for row in result_details["unexpected_rows"]]
+        comparison_values = (
+            [row[comparison_col_name] for row in result_details["missing_rows"]]
+            if comparison_col_name
+            else []
+        )
+        base_values = (
+            [row[base_col_name] for row in result_details["unexpected_rows"]]
+            if base_col_name
+            else []
+        )
         renderer_configuration: RendererConfiguration = RendererConfiguration(
             configuration=configuration,
             result=result,
