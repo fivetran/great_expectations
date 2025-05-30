8ff6b64f20e1be9094a8c492b235201b3f356fb2
Farmer <nathan@greatexpectations.io>
2025-05-30 13:49:06 -0600

    Filter UserWarning not DeprecationWarning

--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -25,7 +25,6 @@ def files_with_deprecation_warnings() -> List[str]:
         "great_expectations/**/*.py", recursive=True
     )
     files_to_exclude = [
-        "great_expectations/compatibility/aws.py",
         "great_expectations/compatibility/docstring_parser.py",
         "great_expectations/compatibility/pyspark.py",
         "great_expectations/compatibility/sqlalchemy_and_pandas.py",
