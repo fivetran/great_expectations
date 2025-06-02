f67d327662fc856084c3565532bf3f30dfb147fb
Farmer <nathan@greatexpectations.io>
2025-06-02 10:08:40 -0600

    Filter UserWarning from pip

--- great_expectations/compatibility/pip.py
+++ great_expectations/compatibility/pip.py
@@ -1,16 +1,19 @@
-"""Pip compatibility module for replacing pkg_resources.parse_requirements functionality."""
+import warnings
 
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=UserWarning, module="pip")
+
     try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import (
-            PipSession,  # noqa: F401  # type: ignore[import-not-found]
-        )
-        from pip._internal.req import parse_requirements  # noqa: F401  # type: ignore[no-redef]
+        # pip >=20
+        from pip._internal.network.session import PipSession
+        from pip._internal.req import parse_requirements
     except ImportError:
-        # pip <= 9.0.3
-        pass
+        try:
+            # 10.0.0 <= pip <= 19.3.1
+            from pip._internal.download import (
+                PipSession,  # noqa: F401  # type: ignore[import-not-found]
+            )
+            from pip._internal.req import parse_requirements  # noqa: F401  # type: ignore[no-redef]
+        except ImportError:
+            # pip <= 9.0.3
+            pass
