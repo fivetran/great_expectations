cf5bbc45e4f75d61e0fad6ff918ac17be1f462c3
Farmer <nathan@greatexpectations.io>
2025-06-03 15:17:47 -0600

    Only ignore warning for PipSession

--- great_expectations/compatibility/pip.py
+++ great_expectations/compatibility/pip.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import warnings
 
 from great_expectations.compatibility.not_imported import NotImported
@@ -27,36 +25,36 @@ with warnings.catch_warnings():
             except ImportError:
                 PipSession = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
 
+try:
+    # pip >=20
+    from pip._internal.req import parse_requirements
+except ImportError:
     try:
-        # pip >=20
+        # 10.0.0 <= pip <= 19.3.1
         from pip._internal.req import parse_requirements
     except ImportError:
         try:
-            # 10.0.0 <= pip <= 19.3.1
-            from pip._internal.req import parse_requirements
+            # pip <= 9.0.3
+            from pip.req import (  # type: ignore[import-not-found, no-redef]
+                parse_requirements,
+            )
         except ImportError:
-            try:
-                # pip <= 9.0.3
-                from pip.req import (  # type: ignore[import-not-found, no-redef]
-                    parse_requirements,
-                )
-            except ImportError:
-                parse_requirements = PIP_NOT_IMPORTED
+            parse_requirements = PIP_NOT_IMPORTED
 
+try:
+    # pip >=20
+    from pip._internal.req.req_install import InstallRequirement
+except ImportError:
     try:
-        # pip >=20
-        from pip._internal.req.req_install import InstallRequirement
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.req.req_install import (
+            InstallRequirement,
+        )
     except ImportError:
         try:
-            # 10.0.0 <= pip <= 19.3.1
-            from pip._internal.req.req_install import (
+            # pip <= 9.0.3
+            from pip.req.req_install import (  # type: ignore[import-not-found, no-redef]
                 InstallRequirement,
             )
         except ImportError:
-            try:
-                # pip <= 9.0.3
-                from pip.req.req_install import (  # type: ignore[import-not-found, no-redef]
-                    InstallRequirement,
-                )
-            except ImportError:
-                InstallRequirement = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
+            InstallRequirement = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
