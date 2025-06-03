7e3e39be885ed07253a16fbded04bed5111962df
Farmer <nathan@greatexpectations.io>
2025-06-03 12:46:59 -0600

    Incorporate NotImported type in compatibility layer

--- great_expectations/compatibility/pip.py
+++ great_expectations/compatibility/pip.py
@@ -1,4 +1,10 @@
+from __future__ import annotations
+
 import warnings
+
+from great_expectations.compatibility.not_imported import NotImported
+
+PIP_NOT_IMPORTED = NotImported("An unsupported version of pip is installed")
 
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=UserWarning, module="_distutils_hack")
@@ -6,19 +12,33 @@ with warnings.catch_warnings():
     try:
         # pip >=20
         from pip._internal.network.session import PipSession
-        from pip._internal.req import parse_requirements
     except ImportError:
         try:
             # 10.0.0 <= pip <= 19.3.1
             from pip._internal.download import (  # type: ignore[import-not-found, no-redef]
                 PipSession,
             )
+        except ImportError:
+            try:
+                # pip <= 9.0.3
+                from pip.download import (  # type: ignore[import-not-found, no-redef]
+                    PipSession,
+                )
+            except ImportError:
+                PipSession = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
+
+    try:
+        # pip >=20
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        try:
+            # 10.0.0 <= pip <= 19.3.1
             from pip._internal.req import parse_requirements  # type: ignore[no-redef]
         except ImportError:
-            # pip <= 9.0.3
-            from pip.download import (  # type: ignore[import-not-found, no-redef]
-                PipSession,  # noqa: F401
-            )
-            from pip.req import (  # type: ignore[import-not-found, no-redef]
-                parse_requirements,  # noqa: F401
-            )
+            try:
+                # pip <= 9.0.3
+                from pip.req import (  # type: ignore[import-not-found, no-redef]
+                    parse_requirements,
+                )
+            except ImportError:
+                parse_requirements = PIP_NOT_IMPORTED
