e9edeb21e661e9032546e8b95b0f2abfe3d7b25d
Farmer <nathan@greatexpectations.io>
2025-06-04 09:39:32 -0600

    Only support pip > 20

--- great_expectations/compatibility/pip.py
+++ great_expectations/compatibility/pip.py
@@ -11,50 +11,16 @@ with warnings.catch_warnings():
         # pip >=20
         from pip._internal.network.session import PipSession
     except ImportError:
-        try:
-            # 10.0.0 <= pip <= 19.3.1
-            from pip._internal.download import (  # type: ignore[import-not-found, no-redef]
-                PipSession,
-            )
-        except ImportError:
-            try:
-                # pip <= 9.0.3
-                from pip.download import (  # type: ignore[import-not-found, no-redef]
-                    PipSession,
-                )
-            except ImportError:
-                PipSession = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
+        PipSession = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
 
 try:
     # pip >=20
     from pip._internal.req import parse_requirements
 except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        try:
-            # pip <= 9.0.3
-            from pip.req import (  # type: ignore[import-not-found, no-redef]
-                parse_requirements,
-            )
-        except ImportError:
-            parse_requirements = PIP_NOT_IMPORTED
+    parse_requirements = PIP_NOT_IMPORTED
 
 try:
     # pip >=20
     from pip._internal.req.req_install import InstallRequirement
 except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.req.req_install import (
-            InstallRequirement,
-        )
-    except ImportError:
-        try:
-            # pip <= 9.0.3
-            from pip.req.req_install import (  # type: ignore[import-not-found, no-redef]
-                InstallRequirement,
-            )
-        except ImportError:
-            InstallRequirement = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
+    InstallRequirement = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
