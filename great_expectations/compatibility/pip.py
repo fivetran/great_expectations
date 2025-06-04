bda2751ba36302b8efa1f01727fd62fbfd2e328a
Farmer <nathan@greatexpectations.io>
2025-06-04 09:39:59 -0600

    Only support pip > 20

--- great_expectations/compatibility/pip.py
+++ great_expectations/compatibility/pip.py
@@ -8,19 +8,16 @@ with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=UserWarning, module="_distutils_hack")
 
     try:
-        # pip >=20
         from pip._internal.network.session import PipSession
     except ImportError:
         PipSession = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
 
 try:
-    # pip >=20
     from pip._internal.req import parse_requirements
 except ImportError:
     parse_requirements = PIP_NOT_IMPORTED
 
 try:
-    # pip >=20
     from pip._internal.req.req_install import InstallRequirement
 except ImportError:
     InstallRequirement = PIP_NOT_IMPORTED  # type: ignore[misc, assignment]
