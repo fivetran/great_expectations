deadc0888ebf3e27257cd03750b18ab72860ef10
Ship <michael.shiplet@greatexpectations.io>
2025-06-24 13:51:14 -0600

    [MAINTENANCE] remove TableFactory from cloud e2e

--- tests/integration/cloud/end_to_end/test_snowflake_datasource.py
+++ tests/integration/cloud/end_to_end/test_snowflake_datasource.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-import os
 import uuid
 from typing import TYPE_CHECKING, Final, Generator
 
+import pandas as pd
 import pytest
 
 from great_expectations import ValidationDefinition
 from great_expectations.core.batch_definition import BatchDefinition
+from tests.integration.conftest import parameterize_batch_for_data_sources
+from tests.integration.test_utils.data_source_config.snowflake import SnowflakeDatasourceTestConfig
 
 if TYPE_CHECKING:
     from great_expectations.checkpoint.checkpoint import Checkpoint, CheckpointResult
@@ -15,60 +17,57 @@ if TYPE_CHECKING:
     from great_expectations.data_context import CloudDataContext
     from great_expectations.datasource.fluent import (
         DataAsset,
-        SnowflakeDatasource,
     )
     from great_expectations.datasource.fluent.sql_datasource import TableAsset
-    from tests.integration.cloud.end_to_end.conftest import TableFactory
 
 RANDOM_SCHEMA: Final[str] = f"i{uuid.uuid4().hex}"
 
 
-@pytest.fixture(scope="module")
-def connection_string() -> str:
-    if os.getenv("SNOWFLAKE_CI_USER_PASSWORD") and os.getenv("SNOWFLAKE_CI_ACCOUNT"):
-        return (
-            "snowflake://ci:${SNOWFLAKE_CI_USER_PASSWORD}@oca29081.us-east-1/ci"
-            f"/{RANDOM_SCHEMA}?warehouse=ci&role=ci"
-        )
-    elif os.getenv("SNOWFLAKE_USER") and os.getenv("SNOWFLAKE_CI_ACCOUNT"):
-        return (
-            "snowflake://${SNOWFLAKE_USER}@oca29081.us-east-1/DEMO_DB"
-            f"/{RANDOM_SCHEMA}?warehouse=COMPUTE_WH&role=PUBLIC&authenticator=externalbrowser"
-        )
-    else:
-        pytest.skip("no snowflake credentials")
+# @pytest.fixture(scope="module")
+# def connection_string() -> str:
+#     if os.getenv("SNOWFLAKE_CI_USER_PASSWORD") and os.getenv("SNOWFLAKE_CI_ACCOUNT"):
+#         return (
+#             "snowflake://ci:${SNOWFLAKE_CI_USER_PASSWORD}@oca29081.us-east-1/ci"
+#             f"/{RANDOM_SCHEMA}?warehouse=ci&role=ci"
+#         )
+#     elif os.getenv("SNOWFLAKE_USER") and os.getenv("SNOWFLAKE_CI_ACCOUNT"):
+#         return (
+#             "snowflake://${SNOWFLAKE_USER}@oca29081.us-east-1/DEMO_DB"
+#             f"/{RANDOM_SCHEMA}?warehouse=COMPUTE_WH&role=PUBLIC&authenticator=externalbrowser"
+#         )
+#     else:
+#         pytest.skip("no snowflake credentials")
+
+
+# @pytest.fixture(scope="module")
+# def datasource(
+#     context: CloudDataContext,
+#     datasource_name: str,
+#     connection_string: str,
+# ) -> SnowflakeDatasource:
+#     """Test Adding and Updating the Datasource associated with this module.
+#     Note: There is no need to test Get or Delete Datasource.
+#     Those assertions can be found in the datasource_name fixture."""
+#     datasource = context.data_sources.add_snowflake(
+#         name=datasource_name,
+#         connection_string=connection_string,
+#         create_temp_table=False,
+#     )
+#     return datasource
 
 
 @pytest.fixture(scope="module")
-def datasource(
-    context: CloudDataContext,
-    datasource_name: str,
-    connection_string: str,
-) -> SnowflakeDatasource:
-    """Test Adding and Updating the Datasource associated with this module.
-    Note: There is no need to test Get or Delete Datasource.
-    Those assertions can be found in the datasource_name fixture."""
-    datasource = context.data_sources.add_snowflake(
-        name=datasource_name,
-        connection_string=connection_string,
-        create_temp_table=False,
-    )
-    return datasource
-
-
-@pytest.fixture(scope="module")
+@parameterize_batch_for_data_sources(
+    data_source_configs=[SnowflakeDatasourceTestConfig()],
+    data=pd.DataFrame({"a": [1, 2]}),
+)
 def data_asset(
-    datasource: SnowflakeDatasource,
-    table_factory: TableFactory,
+    batch_for_datasource,
 ) -> Generator[DataAsset, None, None]:
     """Test the entire Data Asset CRUD lifecycle here and in Data Asset-specific fixtures."""
     asset_name = f"da_{uuid.uuid4().hex}"
     table_name = f"i{uuid.uuid4().hex}"
-    table_factory(
-        gx_engine=datasource.get_execution_engine(),
-        table_names={table_name},
-        schema_name=RANDOM_SCHEMA,
-    )
+    datasource = batch_for_datasource.datasource
     yield datasource.add_table_asset(
         name=asset_name,
         table_name=table_name,
