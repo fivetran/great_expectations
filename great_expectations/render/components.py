45acf500971a3ab5db88a28ec1336f3a34f86f5f
Lukasz Lempart <llempart@gmail.com>
2025-06-26 11:37:53 -0700

    Update components.py
    oops

--- great_expectations/render/components.py
+++ great_expectations/render/components.py
@@ -141,7 +141,7 @@ class RenderedContent:
         return self.to_json_dict() == other.to_json_dict()
 
     @override
-    (self) -> int:
+    __hash__(self) -> int:
         return hash(str(self.to_json_dict()))
 
     @classmethod
