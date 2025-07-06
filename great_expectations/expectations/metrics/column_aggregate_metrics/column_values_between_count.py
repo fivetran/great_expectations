e075613016e519fd34239ef261b05e773de14162
Alan Skelnik <alan.sk97@outlook.com>
2025-07-06 21:19:47 +0200

    change pyspark column reference from DataFrame.__getitem__ to F.col()

--- great_expectations/expectations/metrics/column_aggregate_metrics/column_values_between_count.py
+++ great_expectations/expectations/metrics/column_aggregate_metrics/column_values_between_count.py
@@ -4,6 +4,7 @@ from typing import Any, Dict
 
 import numpy as np
 
+from great_expectations.compatibility.pyspark import functions as F
 from great_expectations.compatibility.sqlalchemy import sqlalchemy as sa
 from great_expectations.core.metric_domain_types import MetricDomainTypes
 from great_expectations.core.util import get_sql_dialect_floating_point_infinity_value
@@ -208,7 +209,7 @@ class ColumnValuesBetweenCount(MetricProvider):
         ) = execution_engine.get_compute_domain(
             domain_kwargs=metric_domain_kwargs, domain_type=MetricDomainTypes.COLUMN
         )
-        column = df[accessor_domain_kwargs["column"]]
+        column = F.col(accessor_domain_kwargs["column"])
 
         if min_value is not None and max_value is not None and min_value > max_value:
             raise ValueError("min_value cannot be greater than max_value")  # noqa: TRY003 # FIXME CoP
