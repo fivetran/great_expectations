06996a2e4b38d41218a6b3dfd908569a573e1052
jmcorreia <jmcorreia1995@gmail.com>
2025-07-08 15:22:48 +0100

    add check for cloud_data_context, similar to abstract data context

--- great_expectations/data_context/data_context/cloud_data_context.py
+++ great_expectations/data_context/data_context/cloud_data_context.py
@@ -144,8 +144,9 @@ class CloudDataContext(SerializableDataContext):
     @override
     def _init_analytics(self) -> None:
         organization_id = self.ge_cloud_config.organization_id
+        analytics_enabled = self._determine_analytics_enabled()
         init_analytics(
-            enable=self._determine_analytics_enabled(),
+            enable=analytics_enabled,
             user_id=self._get_cloud_user_id(),
             data_context_id=self._data_context_id,
             organization_id=uuid.UUID(organization_id) if organization_id else None,
