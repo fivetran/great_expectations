72da7de7cfd55dd4d3e0a0944527d8a832ab5b86
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2025-09-12 08:49:37 -0700

    test again

--- docs/docusaurus/docs/cloud/data_sources/manage_data_sources.py
+++ docs/docusaurus/docs/cloud/data_sources/manage_data_sources.py
@@ -5,36 +5,3 @@ To test, run:
 pytest --docs-tests -k "cloud_docs_manage_data_sources" tests/integration/test_script_runner.py
 """
 
-# EXAMPLE SCRIPT STARTS HERE:
-# <snippet name="docs/docusaurus/docs/cloud/connect/manage_data_sources.py - get cloud context">
-import great_expectations as gx
-
-context = gx.get_context(mode="cloud")
-# </snippet>
-
-# <snippet name="docs/docusaurus/docs/cloud/connect/manage_data_sources.py - verify context type">
-print(type(context).__name__)
-# </snippet>
-
-# <snippet name="docs/docusaurus/docs/cloud/connect/manage_data_sources.py - pick source">
-# The name identitifes which Data Source to update.
-# You cannot change the Data Source name with the API.
-data_source_name = "S3 Data Source"
-# </snippet>
-
-# <snippet name="docs/docusaurus/docs/cloud/connect/manage_data_sources.py - define source updates">
-# You can change some or all connection details.
-bucket_name = "my-new-bucket"
-boto3_options = {
-    "aws_access_key_id": "${MY_NEW_S3_KEY_ID}",
-    "aws_secret_access_key": "${MY_NEW_S3_SECRET_KEY}",
-}
-# </snippet>
-
-
-
-# <snippet name="docs/docusaurus/docs/cloud/connect/manage_data_sources.py - update source>
-context.data_sources.update_pandas_s3(
-    name=data_source_name, bucket=bucket_name, boto3_options=boto3_options
-)
-# </snippet>
