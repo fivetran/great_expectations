c3c68417b279ea617b4fccc42437f7668519abf4
Farmer <nathan@greatexpectations.io>
2025-10-29 11:48:52 -0600

    Add retries to curl command

--- scripts/install_mssql_odbc_driver.sh
+++ scripts/install_mssql_odbc_driver.sh
@@ -10,7 +10,7 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 
 # Download the package to configure the Microsoft repo
-curl -sSL -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)/packages-microsoft-prod.deb
+curl -sSL --retry 2 -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)/packages-microsoft-prod.deb
 
 sudo dpkg --force-confdef --force-confold -i packages-microsoft-prod.deb
 # Delete the file
