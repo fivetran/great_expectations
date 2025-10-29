3cd2c7b4bc9cf4ef0dd71f53518927a1c734053e
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2025-10-29 13:15:14 -0700

    import:

--- great_expectations/expectations/conditions.py
+++ great_expectations/expectations/conditions.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING, Any, Iterable, List, Literal, Union
 
 from great_expectations.compatibility.pydantic import BaseModel, Field, validator
 from great_expectations.compatibility.typing_extensions import override
+from great_expectations._docs_decorators import public_api
+
 
 if TYPE_CHECKING:
     from typing_extensions import TypeAlias
