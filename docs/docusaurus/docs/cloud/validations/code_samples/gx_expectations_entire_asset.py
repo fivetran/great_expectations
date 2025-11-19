c53600448a4757ef8528c40284e7164f95e7af4b
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2025-11-19 11:31:22 -0800

    remove setup

--- docs/docusaurus/docs/cloud/validations/code_samples/gx_expectations_entire_asset.py
+++ docs/docusaurus/docs/cloud/validations/code_samples/gx_expectations_entire_asset.py
@@ -5,22 +5,6 @@ To test, run:
 pytest --docs-tests -k "cloud_docs_gx_expectations_entire_asset" tests/integration/test_script_runner.py
 """
 
-
-def set_up_context_for_example(context):
-    # Create the Data Source
-    source_folder = "./data/folder_with_data"
-    data_source_name = "my_data_source"
-    data_source = context.data_sources.add_pandas_filesystem(
-        name=data_source_name, base_directory=source_folder
-    )
-    assert data_source.name == data_source_name
-
-    # Add a Data Asset
-    asset_name = "my_data_asset"
-    data_asset = data_source.add_csv_asset(name=asset_name)
-    assert data_asset.name == asset_name
-
-
 # EXAMPLE SCRIPT STARTS HERE:
 # <snippet name="docs/docusaurus/docs/cloud/validations/code_samples/gx_expectations_entire_asset.py - define asset">
 data_asset_name = "my_data_asset"
