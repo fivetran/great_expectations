d115c812e4dfc5cb79ddf9336a6ea5a95dca1329
Nevin Tan  <nevin.tan@greatexpectations.io>
2025-11-21 13:01:37 -0500

    Move line

--- docs/docusaurus/docs/cloud/alerts/_examples/create_a_custom_action.py
+++ docs/docusaurus/docs/cloud/alerts/_examples/create_a_custom_action.py
@@ -67,10 +67,10 @@ class MyCustomAction(ValidationAction):
 
         # Replace with your Jira instance URL and Jira Personal Access Token (PAT)
         # host = self.jira_api_url
+        # jira_email_address = "<YOUR_JIRA_EMAIL_ADDRESS>"
         # pat = "<YOUR-PERSONAL-ACCESS-TOKEN>"
 
         # Create a Jira connection using the JIRA library
-        # jira_email_address = "<YOUR_JIRA_EMAIL_ADDRESS>"
         # jira = JIRA(server=host, basic_auth=(jira_email_address, pat))
 
         # Issue data (replace with your own data)
