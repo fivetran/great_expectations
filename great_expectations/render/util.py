7dda15b1b7fb8b320fa62822b9335608163ab5ba
Farmer <nathan@greatexpectations.io>
2025-12-22 12:09:38 -0700

    Use TypeVar

--- great_expectations/render/util.py
+++ great_expectations/render/util.py
@@ -438,9 +438,11 @@ def _convert_unexpected_indices_to_df(
         return pd.DataFrame()
 
     # 1. groupby on domain columns, and turn id/pk into list
-    T = TypeVar("T")
+    # TypeVar constrained to pandas-compatible types (str, int, float, bool, etc.)
+    # Using object as bound since pandas Series can hold various types
+    T = TypeVar("T", bound=object)
 
-    def _agg_func(y: pd.Series[T]) -> list[T]:
+    def _agg_func(y: pd.Series[T]) -> list[T]:  # type: ignore[type-var]  # not yet supported by pandas-stubs
         return list(y)
 
     all_unexpected_indices: pd.DataFrame = unexpected_index_df.groupby(domain_column_name_list).agg(
