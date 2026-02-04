d70901a1424acdc4374e539751652c336d7f3330
Farmer <nathan@greatexpectations.io>
2026-02-04 08:19:30 -0700

    fix: revert be_in_set integration test to original value_counts format

--- tests/integration/data_sources_and_expectations/expectations/test_expect_column_distinct_values_to_be_in_set.py
+++ tests/integration/data_sources_and_expectations/expectations/test_expect_column_distinct_values_to_be_in_set.py
@@ -30,9 +30,13 @@ def test_success_complete_results(batch_for_datasource: Batch) -> None:
     result = batch_for_datasource.validate(expectation, result_format=ResultFormat.COMPLETE)
     assert result.success
     assert result.to_json_dict()["result"] == {
-        "observed_value": None,
-        "unexpected_count": 0,
-        "partial_unexpected_list": [],
+        "details": {
+            "value_counts": [
+                {"value": 1, "count": 1},
+                {"value": 2, "count": 3},
+            ]
+        },
+        "observed_value": [1, 2],
     }
 
 
