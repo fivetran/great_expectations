87967ee42437ba00039b8136dd35ac4042ac1e9a
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2026-02-11 16:44:50 -0500

    Remove some datasources from tests

--- tests/integration/data_sources_and_expectations/test_schemas.py
+++ tests/integration/data_sources_and_expectations/test_schemas.py
@@ -3,20 +3,14 @@ import pytest
 
 import great_expectations.expectations as gxe
 from great_expectations import get_context
-from tests.integration.test_utils.data_source_config import (
-    MSSQLDatasourceTestConfig,
-    PostgreSQLDatasourceTestConfig,
-)
 from tests.integration.test_utils.data_source_config.databricks import (
     DatabricksBatchTestSetup,
     DatabricksDatasourceTestConfig,
 )
-from tests.integration.test_utils.data_source_config.mssql import MSSQLBatchTestSetup
-from tests.integration.test_utils.data_source_config.mysql import (
-    MySQLBatchTestSetup,
-    MySQLDatasourceTestConfig,
+from tests.integration.test_utils.data_source_config.mssql import (
+    MSSQLBatchTestSetup,
+    MSSQLDatasourceTestConfig,
 )
-from tests.integration.test_utils.data_source_config.postgres import PostgresBatchTestSetup
 from tests.integration.test_utils.data_source_config.redshift import (
     RedshiftBatchTestSetup,
     RedshiftDatasourceTestConfig,
@@ -69,48 +63,6 @@ class TestSchemaSupport:
     ) -> None:
         batch_setup = MSSQLBatchTestSetup(
             config=MSSQLDatasourceTestConfig(schema_name=schema_name),
-            data=DATA_FRAME,
-            extra_data={},
-            context=get_context(mode="ephemeral"),
-        )
-        with batch_setup.batch_test_context() as batch:
-            expectation = gxe.ExpectTableRowCountToEqual(value=3)
-
-            result = batch.validate(expectation)
-
-            assert result.success
-
-    @pytest.mark.mysql
-    @pytest.mark.parametrize("schema_name", TEST_SCHEMAS)
-    def test_mysql(
-        self,
-        schema_name: str | None,
-    ) -> None:
-        batch_setup = MySQLBatchTestSetup(
-            config=MySQLDatasourceTestConfig(
-                schema_name=schema_name,
-            ),
-            data=DATA_FRAME,
-            extra_data={},
-            context=get_context(mode="ephemeral"),
-        )
-        with batch_setup.batch_test_context() as batch:
-            expectation = gxe.ExpectTableRowCountToEqual(value=3)
-
-            result = batch.validate(expectation)
-
-            assert result.success
-
-    @pytest.mark.postgresql
-    @pytest.mark.parametrize("schema_name", TEST_SCHEMAS)
-    def test_postgres(
-        self,
-        schema_name: str | None,
-    ) -> None:
-        batch_setup = PostgresBatchTestSetup(
-            config=PostgreSQLDatasourceTestConfig(
-                schema_name=schema_name,
-            ),
             data=DATA_FRAME,
             extra_data={},
             context=get_context(mode="ephemeral"),
