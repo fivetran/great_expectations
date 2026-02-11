0736c19fbff04d20efd3da9f7c8574a1f83a9a22
Tyler Hoffman <tyler.hoffman@greatexpectations.io>
2026-02-11 11:20:05 -0500

    Get test passing

---
+++ tests/integration/data_sources_and_expectations/data_sources/test_mssql.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import pytest
+
+import great_expectations.expectations as gxe
+from tests.integration.conftest import parameterize_batch_for_data_sources
+from tests.integration.test_utils.data_source_config import MSSQLDatasourceTestConfig
+
+pytestmark = pytest.mark.unit
+
+DATA_FRAME = pd.DataFrame(
+    {
+        "words": [
+            "apple",
+            "banana",
+            "cherry",
+        ],
+    }
+)
+
+
+class TestMSSQLSchema:
+    @parameterize_batch_for_data_sources(
+        data_source_configs=[MSSQLDatasourceTestConfig()],
+        data=DATA_FRAME,
+    )
+    def test_schema(
+        self,
+        batch_for_datasource,
+    ) -> None:
+        expectation = gxe.ExpectTableRowCountToEqual(value=3)
+
+        result = batch_for_datasource.validate(expectation)
+
+        assert result.success
