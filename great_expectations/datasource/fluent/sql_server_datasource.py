431ab34c0669b77e042cd9195d9be4a5023155e9
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2026-03-03 10:40:22 -0800

    trust server cert

--- great_expectations/datasource/fluent/sql_server_datasource.py
+++ great_expectations/datasource/fluent/sql_server_datasource.py
@@ -178,10 +178,18 @@ class SQLServerDatasource(SQLDatasource):
             Microsoft SQL Server instance is configured to force encryption.
             Otherwise, establish an unencrypted connection.
             - "Mandatory" - Require the connection to be encrypted.
+            Validate the server certificate unless "trust_server_certificate" set to "True".
             Connection will fail if your Microsoft SQL Server instance does not support TLS.
-            - "Strict" - Require the connection to be encrypted and
-            validate the server certificate. Connection will fail if your Microsoft SQL Server
-            instance does not support TLS or the certificate is not valid.
+            If "trust_server_certificate" is set to "False", connection will fail if
+            the certificate is not valid and publicly trusted.
+            - "Strict" - Use TDS 8.0 where encryption begins before the TLS handshake.
+            Require the connection to be encrypted and validate the server certificate.
+            Connection will fail if your Microsoft SQL Server instance does not support TLS
+            or the certificate is not valid and publicly trusted.
+        trust_server_certificate: If you set `encrypt` to `Mandatory`, you can set
+        `trust_server_certificate` to `True` to enable using an encrypted connection
+        without a valid publicly trusted server certificate (default is "False"). This
+        lets you, for example, use a self-signed certificate with an encrypted connection. 
         driver: The name of the ODBC driver your environment uses to connect to
             Microsoft SQL Server. Common values include the following:
             - "ODBC Driver 18 for SQL Server"
