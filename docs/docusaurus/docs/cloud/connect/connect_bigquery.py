b748ecbfdc7f4142bf39de240fbd66155bce90f9
Kristen Lavavej <kristen.lavavej@greatexpectations.io>
2026-03-27 08:01:24 -0700

    code files

---
+++ docs/docusaurus/docs/cloud/connect/connect_bigquery.py
@@ -0,0 +1,56 @@
+"""
+This is an example script for how to connect GX Cloud to BigQuery.
+
+To test, run:
+pytest --docs-tests -k "cloud_docs_connect_bigquery" tests/integration/test_script_runner.py
+"""
+
+# EXAMPLE SCRIPT STARTS HERE:
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - full code example">
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - get cloud context">
+import great_expectations as gx
+
+context = gx.get_context(mode="cloud")
+# </snippet>
+
+# Hide this
+assert type(context).__name__ == "CloudDataContext"
+# Hide this
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - define source">
+data_source_name = "my_bigquery_datasource"
+connection_string = "bigquery://my_project/my_dataset?credentials_path=/my/credentials.json"
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - add source">
+data_source = context.data_sources.add_bigquery(
+   name=data_source_name, connection_string=connection_string
+)
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - define table asset">
+data_asset_name = "my_table_asset"
+table_name = "my_table"
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - add table asset">
+table_data_asset = data_source.add_table_asset(
+   table_name=table_name, name=data_asset_name
+)
+# </snippet>
+
+# Get the updated Data Source
+data_source = context.data_sources.get(data_source_name)
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - define query asset">
+data_asset_name = "my_query_asset"
+query= "SELECT * from my_table WHERE column = 'value'"
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_bigquery.py - add query asset">
+query_data_asset = data_source.add_query_asset(
+   query=query, name=data_asset_name
+)
+# </snippet>
+
+# </snippet>
---
+++ docs/docusaurus/docs/cloud/connect/connect_trino.py
@@ -0,0 +1,42 @@
+"""
+This is an example script for how to connect GX Cloud to Trino.
+
+To test, run:
+pytest --docs-tests -k "cloud_docs_connect_trino" tests/integration/test_script_runner.py
+"""
+
+# EXAMPLE SCRIPT STARTS HERE:
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - full code example">
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - get cloud context">
+import great_expectations as gx
+
+context = gx.get_context(mode="cloud")
+# </snippet>
+
+# Hide this
+assert type(context).__name__ == "CloudDataContext"
+# Hide this
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - define source">
+data_source_name = "my_trino_datasource"
+connection_string = "trino://my_user:@my_host:my_port/my_catalog/my_database"
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - add source">
+data_source = context.data_sources.add_sql(
+   name=data_source_name, connection_string=connection_string
+)
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - define asset">
+data_asset_name = "my_table_asset"
+table_name = "my_table"
+# </snippet>
+
+# <snippet name="docs/docusaurus/docs/cloud/connect/connect_trino.py - add asset">
+data_asset = data_source.add_table_asset(
+   table_name=table_name, name=data_asset_name
+)
+# </snippet>
+
+# </snippet>
