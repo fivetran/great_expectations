f54af7d63fbffcd1e41c92fa7fd10f91dca2e72c
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2026-06-11 20:54:05 +0200

    skip snowflake

--- tests/conftest.py
+++ tests/conftest.py
@@ -455,8 +455,9 @@ def pytest_collection_modifyitems(config, items):
     skipped_backend_marks = {
         "redshift",
         "databricks",
-        "bigquery"
-    }  # "snowflake",  "athena"
+        "bigquery",
+        "snowflake",
+    }  # "athena"
     for item in items:
         present = skipped_backend_marks.intersection(item.keywords)
         if present:
