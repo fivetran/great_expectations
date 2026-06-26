fb4978616ad2fa364d7c348a7a1bdcfed8c38bf7
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2026-06-26 14:55:03 +0200

    release_prep

--- docs/docusaurus/docusaurus.config.js
+++ docs/docusaurus/docusaurus.config.js
@@ -273,7 +273,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.18.1',
+              label: '1.18.2',
             },
             ['0.18']: {
               label: '0.18.21',
